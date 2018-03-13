8f81ab71d8714e1ad75113869803ad3277a6101c
etienne <erobert@scortex.io>
2018-03-13 15:38:38 +0100

    setup: replace source by . for fish 2.0 compatibility

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
+# As of fish 2.2.0, the . command is deprecated and source should be used instead.
 if test -e ~/.alias
-    source ~/.alias
+    . ~/.alias
 end
 
 alias mkls="make 2> tmp; or less tmp"
