42fd3c5ee2274104b8f19358d0459f39e63a0aab
Etienne ROBERT <etrobert@e1r2p5.42.fr>
2018-05-08 16:02:40 +0200

    install_essentials: add comment to install vundle and improve command

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # stow needs to be installed
-# vundle has to be installed manually
 
-stow alias profile zsh bash git ssh vim tmux
+stow alias profile zsh bash git ssh vim tmux && \
 chmod 600 ssh/.ssh/config
+
+# vundle has to be installed manually
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
