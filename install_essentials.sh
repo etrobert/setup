a86e8a2fb571aac2328d946f46eb8265ed41cad8
erobert <etiennerobert33@gmail.com>
2018-10-24 15:24:51 +0200

    add fish to essentials

--- install_essentials.sh
+++ install_essentials.sh
@@ -2,7 +2,7 @@
 
 # stow needs to be installed
 
-stow alias profile zsh bash git ssh vim tmux && \
+stow alias profile fish zsh bash git ssh vim tmux && \
 chmod 600 ssh/.ssh/config
 
 # vundle has to be installed manually
