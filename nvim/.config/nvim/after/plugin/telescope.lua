150f1c53bfb82169a49f49c1c99cfa72285041e4
Etienne Robert <etiennerobert33@gmail.com>
2024-03-01 13:32:31 +0100

    Add live_grep with <leader>fg

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,15 @@
 local builtin = require('telescope.builtin')
+
+-- Source: https://github.com/nvim-telescope/telescope.nvim/issues/855
+require("telescope").setup {
+    pickers = {
+        live_grep = {
+            file_ignore_patterns = {'.git'},
+            additional_args = {'--hidden'}
+        }
+    }
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
