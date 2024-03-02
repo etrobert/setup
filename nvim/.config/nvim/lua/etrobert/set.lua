3668f7404d665645d7462a3e606e4875fafa0f20
Etienne Robert <etiennerobert33@gmail.com>
2024-03-02 15:46:49 +0100

    nvim: refine settings

--- nvim/.config/nvim/lua/etrobert/set.lua
+++ nvim/.config/nvim/lua/etrobert/set.lua
@@ -5,3 +5,17 @@ vim.opt.number = true
 
 -- Highlights the column 81
 vim.opt.colorcolumn = "81"
+
+vim.opt.guicursor = ""
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 3
