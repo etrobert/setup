766e9261dbb8636a200c2fd5d9717ce143bf196e
Etienne Robert <etiennerobert33@gmail.com>
2024-06-01 18:14:13 +0200

    nvim: Add default keymaps for gitsigns

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -1,20 +1,30 @@
 -- Source: https://github.com/stevearc/conform.nvim
 
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { { "prettierd", "prettier" } },
-		javascriptreact = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettierd", "prettier" } },
-		json = { { "prettierd", "prettier" } },
-		html = { { "prettierd", "prettier" } },
-		markdown = { { "prettierd", "prettier" } },
-	},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Use a sub-list to run only the first available formatter
+
+    -- javascript = { { "prettierd", "prettier" } },
+    -- javascriptreact = { { "prettierd", "prettier" } },
+    -- typescript = { { "prettierd", "prettier" } },
+    -- typescriptreact = { { "prettierd", "prettier" } },
+    -- json = { { "prettierd", "prettier" } },
+    -- html = { { "prettierd", "prettier" } },
+    -- markdown = { { "prettierd", "prettier" } },
+
+    javascript = { { "biome" } },
+    javascriptreact = { { "biome" } },
+    typescript = { { "biome" } },
+    typescriptreact = { { "biome" } },
+    json = { { "biome" } },
+    html = { { "biome" } },
+    markdown = { { "biome" } },
+
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 })
