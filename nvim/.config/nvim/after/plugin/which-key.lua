ad6b96aa57ae99bbcb288df630630039ee6e69f6
Etienne Robert <etiennerobert33@gmail.com>
2024-06-06 13:29:26 +0200

    nvim: Add buffer shortcuts

--- nvim/.config/nvim/after/plugin/which-key.lua
+++ nvim/.config/nvim/after/plugin/which-key.lua
@@ -3,4 +3,10 @@ local wk = require("which-key")
 wk.register({
 	pv = { vim.cmd.Ex, "File Explorer" },
 	n = { vim.cmd.nohlsearch, "Remove Search Highlight" },
+	b = {
+		name = "Buffer",
+		n = { ":bnext<CR>", "Next Buffer" },
+		p = { ":bprev<CR>", "Previous Buffer" },
+		d = { ":bd<CR>", "Delete Buffer" },
+	},
 }, { prefix = "<leader>" })
