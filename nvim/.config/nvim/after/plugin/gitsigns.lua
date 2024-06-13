b8a2583eaf9d9120ded82d4254e30fe382e10b9b
Etienne Robert <etiennerobert33@gmail.com>
2024-06-13 11:19:06 +0200

    nvim: gitsigns: add hunk navigation shortcuts

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,10 +1,33 @@
 require("gitsigns").setup({
 	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 
-	on_attach = function()
+	on_attach = function(bufnr)
 		local gitsigns = require("gitsigns")
 
 		local wk = require("which-key")
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+			end
+		end)
+
+		map("n", "[c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+			end
+		end)
 
 		wk.register({
 			h = {
