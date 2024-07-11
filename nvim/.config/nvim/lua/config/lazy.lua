a84ac1b338298c4ce3f3cfb546e94ab155a33719
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 01:35:52 +0200

    nvim: configure lazy to support structured setup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -16,50 +16,54 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		init = function()
-			vim.cmd("colorscheme catppuccin-macchiato")
-		end,
-	},
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "nvim-treesitter/nvim-treesitter-textobjects" },
-	{ "tpope/vim-fugitive" },
-	{ "lewis6991/gitsigns.nvim" },
-	{ "stevearc/conform.nvim" },
-	{ "numToStr/Comment.nvim", lazy = false },
-	{ "JoosepAlviste/nvim-ts-context-commentstring" },
-	{ "github/copilot.vim" },
-	{ "tpope/vim-surround" },
-	{ "nvim-tree/nvim-web-devicons" },
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			init = function()
+				vim.cmd("colorscheme catppuccin-macchiato")
+			end,
 		},
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+		{ "tpope/vim-fugitive" },
+		{ "lewis6991/gitsigns.nvim" },
+		{ "stevearc/conform.nvim" },
+		{ "numToStr/Comment.nvim", lazy = false },
+		{ "JoosepAlviste/nvim-ts-context-commentstring" },
+		{ "github/copilot.vim" },
+		{ "tpope/vim-surround" },
+		{ "nvim-tree/nvim-web-devicons" },
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			init = function()
+				vim.o.timeout = true
+				vim.o.timeoutlen = 300
+			end,
+			opts = {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+		},
+		{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+		-- LSP Setup
+		{ "VonHeikemen/lsp-zero.nvim" },
+		{ "neovim/nvim-lspconfig" },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "hrsh7th/cmp-nvim-lua" },
+		{ "L3MON4D3/LuaSnip" },
+		{ "rafamadriz/friendly-snippets" },
 	},
-	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
-	-- LSP Setup
-	{ "VonHeikemen/lsp-zero.nvim" },
-	{ "neovim/nvim-lspconfig" },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{ "hrsh7th/cmp-nvim-lua" },
-	{ "L3MON4D3/LuaSnip" },
-	{ "rafamadriz/friendly-snippets" },
 })
