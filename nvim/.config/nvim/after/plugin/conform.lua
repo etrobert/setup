26a58cecb00e4fea74ab2f78072caea7962e355c
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 00:55:52 +0200

    nvim: Simplify and format conform

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -1,30 +1,29 @@
 -- Source: https://github.com/stevearc/conform.nvim
 
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Use a sub-list to run only the first available formatter
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Use a sub-list to run only the first available formatter
 
-    -- javascript = { { "prettierd", "prettier" } },
-    -- javascriptreact = { { "prettierd", "prettier" } },
-    -- typescript = { { "prettierd", "prettier" } },
-    -- typescriptreact = { { "prettierd", "prettier" } },
-    -- json = { { "prettierd", "prettier" } },
-    -- html = { { "prettierd", "prettier" } },
-    -- markdown = { { "prettierd", "prettier" } },
+		-- javascript = { { "prettierd", "prettier" } },
+		-- javascriptreact = { { "prettierd", "prettier" } },
+		-- typescript = { { "prettierd", "prettier" } },
+		-- typescriptreact = { { "prettierd", "prettier" } },
+		-- json = { { "prettierd", "prettier" } },
+		-- html = { { "prettierd", "prettier" } },
+		-- markdown = { { "prettierd", "prettier" } },
 
-    javascript = { { "biome" } },
-    javascriptreact = { { "biome" } },
-    typescript = { { "biome" } },
-    typescriptreact = { { "biome" } },
-    json = { { "biome" } },
-    html = { { "biome" } },
-    markdown = { { "biome" } },
-
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+		javascript = { "biome" },
+		javascriptreact = { "biome" },
+		typescript = { "biome" },
+		typescriptreact = { "biome" },
+		json = { "biome" },
+		html = { "biome" },
+		markdown = { "biome" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
