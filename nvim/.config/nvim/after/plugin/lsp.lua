4975d57ea9795322e87e31126b2f8550aa97c607
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 00:58:16 +0200

    nvim: Configure sourcekit lsp for swift

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -44,7 +44,9 @@ require("mason-lspconfig").setup({
 		lsp_zero.default_setup,
 		lua_ls = function()
 			local lua_opts = lsp_zero.nvim_lua_ls()
-			require("lspconfig").lua_ls.setup(lua_opts)
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup(lua_opts)
+			lspconfig.sourcekit.setup({})
 		end,
 	},
 })
