efc9009291800f205fbedfdd733d02f1b52569ca
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 14:00:46 +0200

    Configure lua_ls lsp

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -8,9 +8,7 @@ return {
 		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		local default_setup = function(server)
-			require("lspconfig")[server].setup({
-				capabilities = lsp_capabilities,
-			})
+			require("lspconfig")[server].setup({ capabilities = lsp_capabilities })
 		end
 
 		return {
@@ -23,6 +21,26 @@ return {
 			},
 			handlers = {
 				default_setup,
+				lua_ls = function()
+					require("lspconfig").lua_ls.setup({
+						capabilities = lsp_capabilities,
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME,
+									},
+								},
+							},
+						},
+					})
+				end,
 			},
 		}
 	end,
