6765f7c39a5d7bc0313eca6b39adcbdd0c485551
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 21:00:18 +0200

    nvim: Configure telescope for git stuff

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -15,7 +15,40 @@ return {
 					b = { builtin.buffers, "Buffers" },
 					h = { builtin.help_tags, "Help Tags" },
 				},
-				gs = { builtin.git_status, " Telescope Git Status" },
+				g = {
+					name = "Git",
+
+					-- Lists git commits with diff preview
+					-- checkout action <cr>
+					-- reset mixed <C-r>m
+					-- reset soft <C-r>s
+					-- reset hard <C-r>h
+					c = { builtin.git_commits, "Commits" },
+
+					-- Lists buffer's git commits with diff preview and checks them out on <cr>
+					B = { builtin.git_bcommits, "Buffer Commits" },
+
+					-- builtin.git_bcommits_range
+					-- Lists buffer's git commits in a range of lines.
+					-- Use options from and to to specify the range.
+					-- In visual mode, lists commits for the selected lines
+
+					-- Lists current changes per file with diff preview and add action. (Multi-selection still WIP)
+					s = { builtin.git_status, "Status" },
+
+					-- Lists all branches with log preview
+					-- checkout action <cr>
+					-- track action <C-t>
+					-- rebase action<C-r>
+					-- create action <C-a>
+					-- switch action <C-s>
+					-- delete action <C-d>
+					-- merge action <C-y>
+					b = { builtin.git_branches, "Branches" },
+
+					-- Lists stash items in current repository with ability to apply them on <cr>
+					S = { builtin.git_stash, "Stash" },
+				},
 			},
 			["<C-p>"] = { builtin.find_files, "Find Files" },
 		})
