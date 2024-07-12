fa551227022f5d87ae6e62acf1a5974721c87bec
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 23:21:09 +0200

    Migrate rest of telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,46 +12,40 @@ return {
 			{ "<leader>fg", builtin.live_grep, desc = "Live Grep" },
 			{ "<leader>fb", builtin.buffers, desc = "Buffers" },
 			{ "<leader>fh", builtin.help_tags, desc = "Help Tags" },
-		})
 
-		wk.register({
-			["<leader>"] = {
-				g = {
-					name = "Git",
+			{ "<leader>g", group = "Git" },
+			-- Lists git commits with diff preview
+			-- checkout action <cr>
+			-- reset mixed <C-r>m
+			-- reset soft <C-r>s
+			-- reset hard <C-r>h
+			{ "<leader>gc", builtin.git_commits, desc = "Commits" },
 
-					-- Lists git commits with diff preview
-					-- checkout action <cr>
-					-- reset mixed <C-r>m
-					-- reset soft <C-r>s
-					-- reset hard <C-r>h
-					c = { builtin.git_commits, "Commits" },
+			-- Lists buffer's git commits with diff preview and checks them out on <cr>
+			{ "<leader>gB", builtin.git_bcommits, desc = "Buffer Commits" },
 
-					-- Lists buffer's git commits with diff preview and checks them out on <cr>
-					B = { builtin.git_bcommits, "Buffer Commits" },
+			-- builtin.git_bcommits_range
+			-- Lists buffer's git commits in a range of lines.
+			-- Use options from and to to specify the range.
+			-- In visual mode, lists commits for the selected lines
 
-					-- builtin.git_bcommits_range
-					-- Lists buffer's git commits in a range of lines.
-					-- Use options from and to to specify the range.
-					-- In visual mode, lists commits for the selected lines
+			-- Lists current changes per file with diff preview and add action. (Multi-selection still WIP)
+			{ "<leader>gs", builtin.git_status, desc = "Status" },
 
-					-- Lists current changes per file with diff preview and add action. (Multi-selection still WIP)
-					s = { builtin.git_status, "Status" },
+			-- Lists all branches with log preview
+			-- checkout action <cr>
+			-- track action <C-t>
+			-- rebase action<C-r>
+			-- create action <C-a>
+			-- switch action <C-s>
+			-- delete action <C-d>
+			-- merge action <C-y>
+			{ "<leader>gb", builtin.git_branches, desc = "Branches" },
 
-					-- Lists all branches with log preview
-					-- checkout action <cr>
-					-- track action <C-t>
-					-- rebase action<C-r>
-					-- create action <C-a>
-					-- switch action <C-s>
-					-- delete action <C-d>
-					-- merge action <C-y>
-					b = { builtin.git_branches, "Branches" },
+			-- Lists stash items in current repository with ability to apply them on <cr>
+			{ "<leader>gS", builtin.git_stash, desc = "Stash" },
 
-					-- Lists stash items in current repository with ability to apply them on <cr>
-					S = { builtin.git_stash, "Stash" },
-				},
-			},
-			["<C-p>"] = { builtin.find_files, "Find Files" },
+			{ "<C-p>", builtin.find_files, desc = "Find Files" },
 		})
 
 		return {
