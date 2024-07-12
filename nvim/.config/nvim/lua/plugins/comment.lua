a1f0a6f5fe7a5c0a6322c8de1ae383fb677cb2ce
Etienne Robert <etiennerobert33@gmail.com>
2024-07-12 04:02:17 +0200

    Change Comment to use opts

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -4,9 +4,9 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 		opts = { enable_autocmd = false },
 	},
-	config = function()
-		require("Comment").setup({
+	opts = function()
+		return {
 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-		})
+		}
 	end,
 }
