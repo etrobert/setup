365f5c071f94f8d196aeebe766a009c887aa8edc
Etienne Robert <etiennerobert33@gmail.com>
2024-07-13 15:29:39 +0200

    nvim: remove which-key dependency from gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -6,8 +6,6 @@ return {
 		on_attach = function(bufnr)
 			local gitsigns = require("gitsigns")
 
-			local wk = require("which-key")
-
 			local function previousHunk()
 				if vim.wo.diff then
 					vim.cmd.normal({ "[c", bang = true })
@@ -24,21 +22,18 @@ return {
 				end
 			end
 
-			wk.add({
-				{ "]c", nextHunk, desc = "Next hunk", buffer = bufnr },
-				{ "[c", previousHunk, desc = "Previous hunk", buffer = bufnr },
+			vim.keymap.set("n", "]c", nextHunk, { buffer = bufnr, desc = "Next hunk" })
+			vim.keymap.set("n", "[c", previousHunk, { buffer = bufnr, desc = "Previous hunk" })
 
-				{ "<leader>h", group = "Hunk" },
-				{ "<leader>hs", gitsigns.stage_hunk, desc = "Stage hunk" },
-				{ "<leader>hr", gitsigns.reset_hunk, desc = "Reset hunk" },
-				{ "<leader>hS", gitsigns.stage_buffer, desc = "Stage buffer" },
-				{ "<leader>hu", gitsigns.undo_stage_hunk, desc = "Undo stage hunk" },
-				{ "<leader>hR", gitsigns.reset_buffer, desc = "Reset buffer" },
-				{ "<leader>hp", gitsigns.preview_hunk, desc = "Preview hunk" },
-				{ "<leader>hd", gitsigns.diffthis, desc = "Diff this" },
+			vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = bufnr, desc = "Stage hunk" })
+			vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = bufnr, desc = "Reset hunk" })
+			vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer, { buffer = bufnr, desc = "Stage buffer" })
+			vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, { buffer = bufnr, desc = "Undo stage hunk" })
+			vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, { buffer = bufnr, desc = "Reset buffer" })
+			vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { buffer = bufnr, desc = "Preview hunk" })
+			vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { buffer = bufnr, desc = "Diff this" })
 
-				{ "<leader>td", gitsigns.toggle_deleted, desc = "Toggle deleted" },
-			})
+			vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted, { buffer = bufnr, desc = "Toggle deleted" })
 		end,
 	},
 }
