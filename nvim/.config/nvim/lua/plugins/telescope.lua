43a4b3921af376e3d599153d56d8d8cfd48aff5f
Etienne Robert <etiennerobert33@gmail.com>
2024-07-13 15:30:06 +0200

    nvim: remove which-key dependency from telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "folke/which-key.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
 		{ "<leader>ff", ":Telescope find_files<CR>", desc = "Find Files" },
 		{ "<leader>fg", ":Telescope live_grep<CR>", desc = "Live Grep" },
