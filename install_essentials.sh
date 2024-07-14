81a22884b649f6c846a2aa8281dd82d83228f030
Etienne Robert <etiennerobert33@gmail.com>
2024-07-14 21:14:14 +0200

    Tentative of improvement for install script

--- install_essentials.sh
+++ install_essentials.sh
@@ -4,3 +4,10 @@
 
 stow alias profile bash readline git ssh vim tmux && \
 chmod 600 ssh/.ssh/config
+
+if [ "$(uname)" == "Darwin" ] ; then
+  # Install brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  brew install --cask alacritty
+fi
