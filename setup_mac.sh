bd45143ea42319119efd971d8197352acd61ba6c
Etienne Robert <etiennerobert33@gmail.com>
2024-07-15 15:25:25 +0200

    Improve messaging

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,20 +1,19 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install Homebrew
+echo "➡ Installing Homebrew"
 if [ -f /opt/homebrew/bin/brew ]; then
   echo "Homebrew is already installed"
 else
-  echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install stow
 /opt/homebrew/bin/brew install stow
 
+echo "➡ Generating SSH key"
 if [ -f ~/.ssh/id_ed25519 ]; then
   echo "SSH key already exists"
 else
-  echo "Generating SSH key"
   ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 fi
