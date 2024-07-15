662016b3a1cedd855c7bf9ee6ae76f4535a85681
Etienne Robert <etiennerobert33@gmail.com>
2024-07-15 16:09:36 +0200

    Install Arc

--- setup_mac.sh
+++ setup_mac.sh
@@ -18,6 +18,26 @@ else
   ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 fi
 
+echo "➡ Installing Arc"
+if [ -d "/Applications/Arc.app" ]; then
+  echo "Arc Browser is already installed"
+else
+  # Step 1: Download the Arc Browser DMG file
+  curl -L "https://releases.arc.net/release/Arc-latest.dmg" -o Arc-latest.dmg
+
+  # Step 2: Mount the DMG file
+  hdiutil attach Arc-latest.dmg
+
+  # Step 3: Copy the application to the Applications folder
+  cp -r /Volumes/Arc/Arc.app /Applications/
+
+  # Step 4: Unmount the DMG file
+  hdiutil detach /Volumes/Arc
+
+  # Step 5: Clean up
+  rm Arc-latest.dmg
+fi
+
 echo "➡ Logging in to GitHub"
 if /opt/homebrew/bin/gh auth status > /dev/null 2>&1; then
   echo "You are already logged in to GitHub CLI."
