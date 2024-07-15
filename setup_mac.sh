5162a307520a7916c0e21f90a8dfffa897412f72
Etienne Robert <etiennerobert33@gmail.com>
2024-07-15 14:45:04 +0200

    if not installed

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,1 +1,7 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew
+if [ -f /opt/homebrew/bin/brew ]; then
+  echo "Homebrew is already installed"
+else
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
