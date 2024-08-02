21959b55a9e8eb0f6537059b2401486d6d619e4f
Etienne Robert <etiennerobert33@gmail.com>
2024-08-02 15:19:05 +0200

    nvim: add docs scrolling shortcuts to cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -21,6 +21,8 @@ return {
 				{ name = "buffer", keyword_length = 3 },
 			},
 			mapping = cmp.mapping.preset.insert({
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
