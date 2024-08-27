09fcced1998a37e34f47e3de9d0e8a9f47e42135
Etienne Robert <etienne.robert@doctolib.com>
2024-08-27 16:08:37 +0200

    nvim: conform: do not fallback to lsp for formatting

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -34,7 +34,7 @@ return {
 		format_on_save = {
 			-- These options will be passed to conform.format()
 			timeout_ms = 500,
-			lsp_format = "fallback",
+			-- lsp_format = "fallback",
 		},
 	},
 }
