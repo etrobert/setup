e5a89c118638700694412f6ffdc7e11db7140791
Etienne Robert <etiennerobert33@gmail.com>
2024-09-10 12:13:24 +0200

    Update packages

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -7,17 +7,17 @@ return {
 			lua = { "stylua" },
 			-- Use a sub-list to run only the first available formatter
 
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
-			-- javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-			-- typescript = { "prettierd", "prettier", stop_after_first = true },
-			-- typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-			-- json = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
 
-			javascript = { "biome" },
-			javascriptreact = { "biome" },
-			typescript = { "biome" },
-			typescriptreact = { "biome" },
-			json = { "biome" },
+			-- javascript = { "biome" },
+			-- javascriptreact = { "biome" },
+			-- typescript = { "biome" },
+			-- typescriptreact = { "biome" },
+			-- json = { "biome" },
 
 			html = { "prettierd", "prettier", stop_after_first = true },
 			markdown = { "prettierd", "prettier", stop_after_first = true },
