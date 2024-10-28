4197d831d0b71f55c6801c8dccc031374e10e40b
Etienne Robert <etiennerobert33@gmail.com>
2024-10-28 22:49:49 +0100

    nvim: create <leader>wd shortcut

--- nvim/.config/nvim/lua/etrobert/remap.lua
+++ nvim/.config/nvim/lua/etrobert/remap.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "File Explorer" })
+vim.keymap.set("n", "<leader>wd", function()
+	vim.cmd("Ex " .. vim.fn.getcwd())
+end, { desc = "File Explorer in Current Working Directory" })
+
 vim.keymap.set("n", "<leader>n", vim.cmd.nohlsearch, { desc = "Remove Search Highlight" })
 
 -- Source: https://lsp-zero.netlify.app/v3.x/blog/you-might-not-need-lsp-zero.html
