655a23512fe5967db7a2f62af9a64305a2f086ee
Etienne Robert <etiennerobert33@gmail.com>
2024-11-01 12:28:53 +0100

    nvim: treesitter textobjects select comment

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -36,11 +36,15 @@ return {
 					-- You can use the capture groups defined in textobjects.scm
 					["af"] = "@function.outer",
 					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
 
+					["ac"] = "@comment.outer",
+					["ic"] = "@comment.inner",
+
+					-- ["ac"] = "@class.outer",
 					-- You can optionally set descriptions to the mappings (used in the desc parameter of
 					-- nvim_buf_set_keymap) which plugins like which-key display
-					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					-- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+
 					-- You can also use captures from other query groups like `locals.scm`
 					["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 				},
