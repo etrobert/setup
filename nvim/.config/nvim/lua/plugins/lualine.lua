974c43c1d3a63d27187ed908b50cccd12059cfac
Etienne Robert <etiennerobert33@gmail.com>
2025-03-23 04:54:41 +0100

    nvim: update lualine config to show relative file path

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,22 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1, -- Show relative path
+				},
+			},
+		},
+		inactive_sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1, -- Show relative path
+				},
+			},
+		},
+	},
 }
