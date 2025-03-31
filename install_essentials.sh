84f50462b0dfd653d73b0b6bc7361e7a1f5005fe
Etienne Robert <etiennerobert33@gmail.com>
2025-04-01 00:13:08 +0200

    update install_essentials script

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
-# stow needs to be installed
+set -e
 
-stow alias profile bash readline git ssh vim tmux &&
+setup_homebrew() {
+  echo "Setting up Homebrew..."
+
+  if command -v brew >/dev/null; then
+    echo "Homebrew is already installed."
+  else
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
+
+setup_stow() {
+  echo "Setting up GNU Stow..."
+
+  if command -v stow >/dev/null; then
+    echo "GNU Stow is already installed."
+  else
+    echo "GNU Stow not found. Installing GNU Stow..."
+    brew install stow
+  fi
+}
+
+setup_dotfiles() {
+  echo "Setting up dotfiles..."
+
+  stow alias bash ghostty git macos nvim profile readline ssh tmux
+
+  echo "Setting proper permissions for SSH config..."
   chmod 600 ssh/.ssh/config
+}
 
-if [ "$(uname)" == "Darwin" ]; then
-  # Install brew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  brew install --cask alacritty
-fi
+setup_homebrew
+setup_stow
+setup_dotfiles
