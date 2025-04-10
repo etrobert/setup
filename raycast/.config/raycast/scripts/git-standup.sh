00e4bd579a7e53a4c33795845706075b81ed67fe
Etienne Robert <etiennerobert33@gmail.com>
2025-04-11 00:25:07 +0200

    raycast: create raycast scripts

---
+++ raycast/.config/raycast/scripts/git-standup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Standup
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 💻
+
+# Documentation:
+# @raycast.author Étienne Robert
+# @raycast.authorURL https://github.com/etrobert
+# @raycast.description Lists your commits from the last 24 hours.
+
+cd "/Users/etienne/work/haii"
+
+git log \
+  --all \
+  --author="$(git config user.name)" \
+  --since=yesterday.midnight \
+  --pretty=format:"%C(yellow)%d%Creset %s %Cblue(%ar)%Creset"
---
+++ raycast/.config/raycast/scripts/meet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title New Google Meet
+# @raycast.mode silent
+# @raycast.icon 📹
+# @raycast.author Étienne Robert
+# @raycast.authorURL https://github.com/etrobert
+
+open https://meet.google.com/new
