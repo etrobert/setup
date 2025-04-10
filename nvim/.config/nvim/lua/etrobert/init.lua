7445bc005a1b90a61f5f9c4fff24899d82c89632
Etienne Robert <etiennerobert33@gmail.com>
2025-04-11 01:17:49 +0200

    welcome screen

--- nvim/.config/nvim/lua/etrobert/init.lua
+++ nvim/.config/nvim/lua/etrobert/init.lua
@@ -1,2 +1,13 @@
 require("etrobert.remap")
 require("etrobert.set")
+
+-- Show welcome screen when Neovim starts
+local welcome = require("etrobert.welcome")
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Only show welcome screen if no arguments were passed to Neovim
+    if vim.fn.argc() == 0 then
+      welcome.setup()
+    end
+  end,
+})
