5060bb968538f01633575a2794dd1c328f61d1b1
Etienne Robert <etiennerobert33@gmail.com>
2025-06-05 22:07:41 +0200

    nvim: highlight yanked text using autocommand for improved visual feedback

--- nvim/.config/nvim/lua/etrobert/set.lua
+++ nvim/.config/nvim/lua/etrobert/set.lua
@@ -40,4 +40,12 @@ vim.o.updatetime = 250
 -- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
 
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	callback = function()
+		vim.hl.on_yank({ higroup = "Visual", timeout = 300 })
+	end,
+})
+
 vim.fn.matchadd("TrailingWhitespace", [[\s\+$]])
