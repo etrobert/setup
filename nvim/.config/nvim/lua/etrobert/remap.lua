1d496cee7436aaa32b825a3e4ed00308d7beb0c4
Etienne Robert <etiennerobert33@gmail.com>
2025-08-28 18:09:55 +0200

    nvim: document global lsp keymaps in remap.lua

--- nvim/.config/nvim/lua/etrobert/remap.lua
+++ nvim/.config/nvim/lua/etrobert/remap.lua
@@ -53,12 +53,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "gD", function()
 			vim.lsp.buf.declaration()
 		end, opts)
-		vim.keymap.set("n", "go", function()
-			vim.lsp.buf.type_definition()
-		end, opts)
-		vim.keymap.set("n", "gs", function()
-			vim.lsp.buf.signature_help()
-		end, opts)
+		-- These GLOBAL keymaps are created unconditionally when Nvim starts:
+		-- - "grn" is mapped in Normal mode to |vim.lsp.buf.rename()|
+		-- - "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
+		-- - "grr" is mapped in Normal mode to |vim.lsp.buf.references()|
+		-- - "gri" is mapped in Normal mode to |vim.lsp.buf.implementation()|
+		-- - "grt" is mapped in Normal mode to |vim.lsp.buf.type_definition()|
+		-- - "gO" is mapped in Normal mode to |vim.lsp.buf.document_symbol()|
+		-- - CTRL-S is mapped in Insert mode to |vim.lsp.buf.signature_help()|
+		-- - "an" and "in" are mapped in Visual mode to outer and inner incremental
+		--  selections, respectively, using |vim.lsp.buf.selection_range()|
 		vim.keymap.set({ "n", "x" }, "<F3>", function()
 			vim.lsp.buf.format({ async = true })
 		end, opts)
