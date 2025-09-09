b3692aa0b99d76035bf238eb52b52511b58240f5
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 23:17:59 +0200

    nvim: move conform config to native-plugins/ directory

---
+++ nvim/.config/nvim/lua/native-plugins/conform.lua
@@ -0,0 +1,38 @@
+vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		html = { "prettierd", "prettier", stop_after_first = true },
+		markdown = { "prettierd", "prettier", stop_after_first = true },
+		css = { "prettierd", "prettier", stop_after_first = true },
+		swift = { "swiftformat" },
+		sh = { "shfmt" },
+		rust = { "rustfmt" },
+	},
+	format_on_save = {
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
+})
+
+vim.keymap.set("", "<leader>fm", function()
+	require("conform").format({ async = true })
+end, { desc = "Format current buffer" })
+
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
