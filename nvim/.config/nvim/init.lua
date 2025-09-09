5e07845da54c7877fb2a70cc8661862d4ebd15fb
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 22:45:55 +0200

    nvim: move catppuccin config to native-plugins/ directory

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,6 @@ require("config.lazy")
 
 vim.opt.packpath:prepend(vim.fn.stdpath("data") .. "/site")
 
+require("native-plugins.catppuccin")
 require("native-plugins.notify")
 require("native-plugins.colorizer")
