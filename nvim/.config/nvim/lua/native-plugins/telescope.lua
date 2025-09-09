9685efbc6ba626a55c03ec77c8fba514740c789d
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 23:41:13 +0200

    nvim: move telescope config to native-plugins/ directory

---
+++ nvim/.config/nvim/lua/native-plugins/telescope.lua
@@ -0,0 +1,63 @@
+vim.pack.add({
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/nvim-telescope/telescope-ui-select.nvim",
+})
+
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules", "%.git%/", "package%-lock.json", "pnpm%-lock.yaml" },
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		grep_string = {
+			additional_args = { "--hidden" },
+		},
+		live_grep = {
+			additional_args = { "--hidden" },
+		},
+	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown(),
+		},
+	},
+})
+
+require("telescope").load_extension("ui-select")
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", require("telescope.multigrep"), { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+vim.keymap.set("n", "<leader>fs", builtin.lsp_workspace_symbols, { desc = "LSP Workspace Symbols" })
+vim.keymap.set("n", "<leader>ft", builtin.builtin, { desc = "Telescope Pickers" })
+vim.keymap.set("n", "<leader>fp", function()
+	builtin.find_files({
+		cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+	})
+end, { desc = "Find File in Lazy Plugins" })
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Grep String" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git Commits" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git Status" })
+vim.keymap.set("n", "<leader>gB", builtin.git_bcommits, { desc = "Git Buffer Commits" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git Branches" })
+vim.keymap.set("n", "<leader>gS", builtin.git_stash, { desc = "Git Stash" })
+vim.keymap.set("n", "<leader>gd", function()
+	builtin.git_commits({
+		git_command = { "git", "log", "--oneline", "main..HEAD" },
+	})
+end, { desc = "Git Branch Diff with Main" })
+vim.keymap.set(
+	"n",
+	"<leader>gf",
+	require("telescope.git-diverged-files").git_diverged_files,
+	{ desc = "Files Changed Since Diverged from Main" }
+)
