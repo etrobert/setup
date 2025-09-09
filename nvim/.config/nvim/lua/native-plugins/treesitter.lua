35568b605b01edf885c9b147f0af5fbe06621731
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 23:47:41 +0200

    nvim: move treesitter config to native-plugins/ directory

---
+++ nvim/.config/nvim/lua/native-plugins/treesitter.lua
@@ -0,0 +1,86 @@
+vim.pack.add({
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+})
+vim.cmd("packadd nvim-treesitter")
+vim.cmd("packadd nvim-treesitter-textobjects")
+
+require("nvim-treesitter.configs").setup({
+	modules = {},
+	ignore_install = {},
+	ensure_installed = {
+		"bash",
+		"c",
+		"diff",
+		"html",
+		"lua",
+		"luadoc",
+		"markdown",
+		"markdown_inline",
+		"query",
+		"vim",
+		"vimdoc",
+		"javascript",
+		"typescript",
+		"json",
+		"go",
+		"rust",
+	},
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@comment.outer",
+				["ic"] = "@comment.inner",
+				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+			},
+			selection_modes = {
+				["@parameter.outer"] = "v",
+				["@function.outer"] = "V",
+				["@class.outer"] = "<c-v>",
+			},
+			include_surrounding_whitespace = true,
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = { query = "@class.outer", desc = "Next class start" },
+				["]o"] = "@loop.*",
+				["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+	},
+})
