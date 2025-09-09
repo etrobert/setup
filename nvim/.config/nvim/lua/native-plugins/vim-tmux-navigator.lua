67abef1ede6b755d7af6dd8eb267cf558b1868d7
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 23:12:06 +0200

    nvim: move vim-tmux-navigator config to native-plugins/ dir

---
+++ nvim/.config/nvim/lua/native-plugins/vim-tmux-navigator.lua
@@ -0,0 +1,8 @@
+vim.pack.add({ "https://github.com/christoomey/vim-tmux-navigator" })
+vim.cmd("packadd vim-tmux-navigator")
+
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>")
