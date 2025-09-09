866a4f22c715b5da372ad6d9c3b2b1bd494c9380
Etienne Robert <etiennerobert33@gmail.com>
2025-09-09 22:33:35 +0200

    nvim: move nvim-notify to pack directory in init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
 require("etrobert")
 
 require("config.lazy")
+
+vim.opt.packpath:prepend(vim.fn.stdpath("data") .. "/site")
+
+vim.pack.add({ "https://github.com/rcarriga/nvim-notify" })
+vim.notify = require("notify")
