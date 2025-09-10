5e799c0dd01f42d4931e9b55ac3dc295c5770b73
Etienne Robert <etiennerobert33@gmail.com>
2025-09-10 10:15:02 +0200

    nvim: move mason-lspconfig config to native-plugins/ dir

---
+++ nvim/.config/nvim/lua/native-plugins/mason-lspconfig.lua
@@ -0,0 +1,34 @@
+vim.pack.add({
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/j-hui/fidget.nvim",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
+})
+
+require("mason").setup({})
+require("fidget").setup({})
+
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"biome",
+		"eslint",
+		"tailwindcss",
+		"ts_ls",
+		"rust_analyzer",
+		"bashls",
+		"gopls",
+		"lua_ls",
+	},
+})
+
+-- TODO: Remove all copilot stuff once its available
+-- with ensure_installed = { "copilot" }
+
+-- Ensure copilot-language-server is installed via Mason
+local mason_registry = require("mason-registry")
+if not mason_registry.is_installed("copilot-language-server") then
+	mason_registry.get_package("copilot-language-server"):install()
+end
+
+vim.lsp.enable("copilot")
+
