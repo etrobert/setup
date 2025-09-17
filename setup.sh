7aaaea22a171c7c77cae44b0f053211875c373d0
Etienne Robert <etiennerobert33@gmail.com>
2025-09-17 23:00:03 +0200

    setup: stow prettier configs with other dotfiles

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ setup_dotfiles() {
   cd "$HOME/setup"
 
   echo "Stowing dotfiles..."
-  stow alias bash ghostty git macos nvim profile readline ssh tmux skhd raycast
+  stow alias bash ghostty git macos nvim profile readline ssh tmux skhd raycast prettier
 
   echo "Setting proper permissions for SSH config..."
   chmod 600 ssh/.ssh/config
