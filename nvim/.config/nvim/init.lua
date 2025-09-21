22f3fca98a9d4d08e97db994de3b0c37b9754f2e
Etienne Robert <etiennerobert33@gmail.com>
2025-09-21 17:07:39 +0200

    nvim: add nvim-spider plugin and motions keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,6 +16,7 @@ vim.pack.add({
 	"https://github.com/windwp/nvim-ts-autotag",
 	"https://github.com/nvim-lualine/lualine.nvim",
 	"https://github.com/folke/which-key.nvim",
+	"https://github.com/chrisgrieser/nvim-spider",
 })
 
 require("catppuccin").setup({ float = { transparent = true, solid = false } })
@@ -53,6 +54,11 @@ require("which-key").setup({
 		{ "<leader>f", group = "Telescope Files" },
 	},
 })
+
+-- nvim-spider
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
 
 require("plugins.harpoon")
 require("plugins.vim-tmux-navigator")
