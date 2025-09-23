0ea96483159f8545f2ef330351eea260840e6aea
Etienne Robert <etiennerobert33@gmail.com>
2025-09-23 16:55:28 +0200

    nvim: enable lsp inline completion back again

--- nvim/.config/nvim/lua/etrobert/set.lua
+++ nvim/.config/nvim/lua/etrobert/set.lua
@@ -56,9 +56,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- vim.opt.list = true
 -- vim.opt.listchars = { tab = "» ", extends = "›", precedes = "‹", nbsp = "·", trail = "·" }
 
--- Disable because super buggy
--- See https://github.com/etrobert/setup/issues/10
--- vim.lsp.inline_completion.enable()
+vim.lsp.inline_completion.enable()
 
 -- don't give the intro message when starting Vim, see :intro
 vim.opt.shortmess:append("I")
