06afa965f54a6d90db23ea12733f5c7eb1b7acaa
Etienne Robert <etiennerobert33@gmail.com>
2025-10-17 19:06:26 +0200

    nvim: disable workspace diagnostics for performance

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -93,15 +93,16 @@ require("plugins.telescope")
 require("plugins.treesitter")
 require("plugins.cmp")
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("workspace-diagnostics", { clear = true }),
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client then
-			require("workspace-diagnostics").populate_workspace_diagnostics(client, args.buf)
-		end
-	end,
-})
+-- Disabled because this takes a monstrous amount of ressources
+-- vim.api.nvim_create_autocmd("LspAttach", {
+-- 	group = vim.api.nvim_create_augroup("workspace-diagnostics", { clear = true }),
+-- 	callback = function(args)
+-- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+-- 		if client then
+-- 			require("workspace-diagnostics").populate_workspace_diagnostics(client, args.buf)
+-- 		end
+-- 	end,
+-- })
 
 -- require("hardtime").setup()
 
