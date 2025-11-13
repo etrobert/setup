2f1d6411c9791ff4a018f049c9221e839e83272e
Etienne Robert <etiennerobert33@gmail.com>
2025-11-13 19:04:30 +0100

    nvim: set catppuccin only for GUI sessions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -35,8 +35,12 @@ vim.pack.add({
 
 vim.cmd.packadd("nvim.undotree")
 
-require("catppuccin").setup({ float = { transparent = true, solid = false } })
-vim.cmd("colorscheme catppuccin-macchiato")
+if os.getenv("WAYLAND_DISPLAY") or vim.fn.has("mac") == 1 then
+	-- Graphical session (Wayland on Linux or macOS)
+	require("catppuccin").setup({ float = { transparent = true, solid = false } })
+	vim.cmd("colorscheme catppuccin-macchiato")
+	-- else we're in a tty, using default theme
+end
 
 -- vim-fugitive
 vim.keymap.set("n", "<leader>ds", ":Gdiffsplit<CR>", { desc = "Git diff split" })
