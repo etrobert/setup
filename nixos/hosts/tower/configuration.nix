d60ed453d1a7fc2567458c31ed5d3c890507c77f
Etienne Robert <etiennerobert33@gmail.com>
2026-01-09 20:23:56 +0100

    tower: move system config options to common module

--- nixos/hosts/tower/configuration.nix
+++ nixos/hosts/tower/configuration.nix
@@ -12,49 +12,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable I2C for ddcutil (external monitor brightness)
-  hardware.i2c.enable = true;
-
-  # Enable bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
   networking.hostName = "tower";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Berlin";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-    options = "ctrl:nocaps";
-  };
-
-  console.useXkbConfig = true; # Apply XKB options (e.g. Caps -> Ctrl)
-
-  programs.zsh.enable = true;
-
-  users.users.soft = {
-    isNormalUser = true;
-    description = "Etienne";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    shell = pkgs.zsh;
-    packages = with pkgs; [ ];
-  };
-
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -1,7 +1,44 @@
-{ ... }:
+{ pkgs, ... }:
 {
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  # Enable I2C for ddcutil (external monitor brightness)
+  hardware.i2c.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+    options = "ctrl:nocaps";
+  };
+
+  console.useXkbConfig = true; # Apply XKB options (e.g. Caps -> Ctrl)
+
+  programs.zsh.enable = true;
+
+  users.users.soft = {
+    isNormalUser = true;
+    description = "Etienne";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
 }
