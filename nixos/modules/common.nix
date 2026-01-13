9c0e26d4d6a20cbc53986c0d3ce5383661cb7176
Etienne Robert <etiennerobert33@gmail.com>
2026-01-13 19:49:31 +0100

    nixos: install ublock origin in firefox

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -35,6 +35,12 @@
     enable = true;
     policies = {
       PasswordManagerEnabled = false;
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
   };
 
