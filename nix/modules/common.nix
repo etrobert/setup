b0f043ee2bc82a44b172554a2a48e4183e8e9765
Etienne Robert <etiennerobert33@gmail.com>
2026-01-19 13:37:51 +0100

    nixos: move unfree options to separate module

--- nix/modules/common.nix
+++ nix/modules/common.nix
@@ -1,87 +1,72 @@
+{ pkgs, pronto, ... }:
 {
-  config,
-  pkgs,
-  pronto,
-  lib,
-  ...
-}:
-{
-  options.allowedUnfreePackages = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
-    default = [ ];
-    description = "List of unfree package names to allow";
-  };
+  imports = [ ./unfree.nix ];
 
-  config = {
-    allowedUnfreePackages = [
-      "claude-code"
-      "discord"
-      "spotify"
-    ];
+  allowedUnfreePackages = [
+    "claude-code"
+    "discord"
+    "spotify"
+  ];
 
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    nixpkgs.config.allowUnfreePredicate =
-      pkg: builtins.elem (lib.getName pkg) config.allowedUnfreePackages;
+  environment.systemPackages = with pkgs; [
+    pronto.packages.${pkgs.stdenv.hostPlatform.system}.default
+    act
+    adwaita-icon-theme
+    audacity
+    bash-language-server
+    bat
+    btop
+    bun
+    claude-code
+    codex
+    difftastic
+    discord
+    entr
+    eza
+    fd
+    firefox
+    fzf
+    gcc
+    gh
+    # ghostty # https://github.com/ghostty-org/ghostty/discussions/4359
+    git
+    gnumake
+    go
+    gopls
+    htop
+    hyperfine # Command-line benchmarking tool
+    imagemagick # for image rendering in nvim using snacks.image
+    jq
+    jqp # TUI playground to experiment with jq
+    libnotify
+    lua-language-server
+    magic-wormhole
+    neovim
+    nixd
+    nixfmt
+    nodejs_latest
+    ollama
+    opencode
+    prettierd
+    ripgrep
+    shellcheck
+    shfmt
+    spotify
+    stow
+    stylua
+    tmux
+    typescript-language-server
+    vim
+    wget
+    yt-dlp
+  ];
 
-    environment.systemPackages = with pkgs; [
-      pronto.packages.${pkgs.stdenv.hostPlatform.system}.default
-      act
-      adwaita-icon-theme
-      audacity
-      bash-language-server
-      bat
-      btop
-      bun
-      claude-code
-      codex
-      difftastic
-      discord
-      entr
-      eza
-      fd
-      firefox
-      fzf
-      gcc
-      gh
-      # ghostty # https://github.com/ghostty-org/ghostty/discussions/4359
-      git
-      gnumake
-      go
-      gopls
-      htop
-      hyperfine # Command-line benchmarking tool
-      imagemagick # for image rendering in nvim using snacks.image
-      jq
-      jqp # TUI playground to experiment with jq
-      libnotify
-      lua-language-server
-      magic-wormhole
-      neovim
-      nixd
-      nixfmt
-      nodejs_latest
-      ollama
-      opencode
-      prettierd
-      ripgrep
-      shellcheck
-      shfmt
-      spotify
-      stow
-      stylua
-      tmux
-      typescript-language-server
-      vim
-      wget
-      yt-dlp
-    ];
+  programs.zsh.enable = true;
 
-    programs.zsh.enable = true;
-
-    fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
-  };
+  fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
 }
---
+++ nix/modules/unfree.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+{
+  options.allowedUnfreePackages = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [ ];
+    description = "List of unfree package names to allow";
+  };
+
+  config.nixpkgs.config.allowUnfreePredicate =
+    pkg: builtins.elem (lib.getName pkg) config.allowedUnfreePackages;
+}
