203d90dd33c4e4182882a3484920ba6393fe07d6
Etienne Robert <etiennerobert33@gmail.com>
2026-01-21 18:41:14 +0100

    linux: enable xdg.mime to fix app launcher desktop files

--- nix/modules/home/linux.nix
+++ nix/modules/home/linux.nix
@@ -8,4 +8,8 @@
   imports = [ ./common.nix ];
 
   home.homeDirectory = "/home/${config.home.username}";
+
+  # Ensures XDG_DATA_DIRS includes the profile share directory
+  # so app launchers (wofi, rofi) can find .desktop files
+  xdg.mime.enable = true;
 }
