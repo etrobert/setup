182bbef5ccea73882d25d5c452d4371ee943b206
Etienne Robert <etiennerobert33@gmail.com>
2026-01-27 00:39:06 +0100

    nixos: add aaron host key and update secrets config

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,17 +2,14 @@ let
   # Machine host public keys (from /etc/ssh/ssh_host_ed25519_key.pub)
   tower = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHagaONxn4Ua5dkPfiGuavydHFfIEUVWMBrZHsucIILT";
   leod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgObi3D4k+OGPizrmEnHVKRcl6tuMsrAyP54LL6SVRi";
-
-  # aaron = "ssh-ed25519 ...";
+  aaron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/Y38NV8a/9rfDq+7W1UFfAFDo8SkwQ5JAl/U24u0ne";
 
   allLinux = [
     tower
     leod
   ];
-  allMachines = allLinux; # ++ [ aaron ];
+  allMachines = allLinux ++ [ aaron ];
 in
 {
-  # Example:
-  # "secret-name.age".publicKeys = allLinux;
   "openai-api-key.age".publicKeys = allMachines;
 }
