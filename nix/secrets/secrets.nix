5a2693d30f10f754da21dfa2192cbb55b8957a74
Etienne Robert <etiennerobert33@gmail.com>
2026-01-27 00:11:18 +0100

    nixos: add leod host key

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,10 +1,14 @@
 let
   # Machine host public keys (from /etc/ssh/ssh_host_ed25519_key.pub)
   tower = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHagaONxn4Ua5dkPfiGuavydHFfIEUVWMBrZHsucIILT";
-  # leod = "ssh-ed25519 ...";
+  leod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgObi3D4k+OGPizrmEnHVKRcl6tuMsrAyP54LL6SVRi";
+
   # aaron = "ssh-ed25519 ...";
 
-  allLinux = [ tower ]; # add leod when available
+  allLinux = [
+    tower
+    leod
+  ];
   allMachines = allLinux; # ++ [ aaron ];
 in
 {
