3b8030549217434cad5f174eaa7aff382c59e09e
Etienne Robert <etiennerobert33@gmail.com>
2026-01-30 19:23:57 +0100

    nvim: add treesitter-textobjects select + keymaps

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -22,6 +22,23 @@ require("nvim-treesitter").install({
 	"rust",
 })
 
+require("nvim-treesitter-textobjects").setup({ select = { lookahead = true } })
+
+local select = require("nvim-treesitter-textobjects.select")
+
+vim.keymap.set({ "x", "o" }, "af", function()
+	select.select_textobject("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "if", function()
+	select.select_textobject("@function.inner", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ac", function()
+	select.select_textobject("@comment.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ic", function()
+	select.select_textobject("@comment.inner", "textobjects")
+end)
+
 -- require("nvim-treesitter.configs").setup({
 -- 	modules = {},
 -- 	ignore_install = {},
