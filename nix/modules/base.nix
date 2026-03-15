a24cda7fab2e633b26eaa4be9038c22431aec71a
Etienne Robert <etiennerobert33@gmail.com>
2026-03-15 12:42:57 +0100

    nix: set trusted-users to root and @wheel

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -12,6 +12,10 @@
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
   };
 
   nix.gc = {
