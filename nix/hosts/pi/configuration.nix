f84ea124f5781e3f4edea5c4c5daa427cae6533c
Etienne Robert <etiennerobert33@gmail.com>
2026-03-16 23:46:10 +0100

    pi: add etiennerobert ed25519 ssh key

--- nix/hosts/pi/configuration.nix
+++ nix/hosts/pi/configuration.nix
@@ -40,6 +40,7 @@
   services.tailscale.extraUpFlags = [ "--advertise-exit-node" ];
 
   users.users.soft.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeCI4pCgGMAB9w8u3l0TLhCKdvi0sSd0AjckDr8/tgD etiennerobert@MacBook-Pro-3.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXiLGjBlPoqRSzJ7KfEyMzJ3JRBqelOepsiL4ri9OqW soft@leod"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8/rx4MPrvwQBU1cy5qkhBgnRALS6Jzc9I20EcBnAx soft@nixos"
   ];
