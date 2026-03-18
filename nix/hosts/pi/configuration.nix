79b2b7afa4fc726f0a7bbbea1e92ebd816d8f909
Etienne Robert <etiennerobert33@gmail.com>
2026-03-18 14:41:35 +0100

    pi: enable navidrome service with custom folders

--- nix/hosts/pi/configuration.nix
+++ nix/hosts/pi/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   environment.systemPackages = [ pkgs.ghostty.terminfo ];
@@ -38,6 +38,19 @@
   networking.networkmanager.enable = true;
 
   services.tailscale.extraUpFlags = [ "--advertise-exit-node" ];
+
+  services.navidrome = {
+    enable = true;
+    settings = {
+      Address = "0.0.0.0";
+      MusicFolder = "/home/soft/sync/music";
+      PlaylistsPath = "/home/soft/sync/playlists";
+      DataFolder = "/home/soft/.local/share/navidrome";
+    };
+    user = "soft";
+  };
+
+  systemd.services.navidrome.serviceConfig.ProtectHome = lib.mkForce "read-only";
 
   users.users.soft.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeCI4pCgGMAB9w8u3l0TLhCKdvi0sSd0AjckDr8/tgD etiennerobert@MacBook-Pro-3.local"
