961c3d0224d40c10827729387ccb5c84e5fcffdb
Etienne Robert <etiennerobert33@gmail.com>
2026-03-19 14:46:19 +0100

    darwin: add Syncthing config to home module

--- nix/modules/nixos-base.nix
+++ nix/modules/nixos-base.nix
@@ -45,6 +45,7 @@
           "leod".id = "5DCR24L-XI2U2AF-7AMMGXE-S4R7TQK-PDOYLGT-5UZLZNV-SERXLIT-BJ6QEAY";
           "tower".id = "3IIJQ3X-2BY72RR-YVNBZBQ-OAB6PM5-SPS3WPG-MCPTFVD-YSQ33SS-X4Q5DA3";
           "pi".id = "EOXLGRM-GCJUBN3-6HD656O-KYXFYEX-N425OIL-SLBL7XJ-VN2RSXW-F7VJMAI";
+          "etiennerobert@aaron".id = "JSQOTCO-D4X3GSX-ECPK33M-22LUG5V-AGG6ARY-EQAP7PB-JGOMRQK-ZRVJDAL";
         };
         folders = {
           "sync" = {
@@ -54,6 +55,7 @@
               "leod"
               "tower"
               "pi"
+              "etiennerobert@aaron"
             ];
             versioning = {
               type = "staggered";
