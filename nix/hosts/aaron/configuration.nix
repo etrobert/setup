b290d27c54ff0f5adc3c95aff02e69ca5907a817
Etienne Robert <etiennerobert33@gmail.com>
2026-03-19 14:26:46 +0100

    aaron: open Raycast at login via launchd agent

--- nix/hosts/aaron/configuration.nix
+++ nix/hosts/aaron/configuration.nix
@@ -130,6 +130,16 @@ in
 
   services.tailscale.enable = true;
 
+  # Open Raycast at login
+  launchd.user.agents.raycast.serviceConfig = {
+    ProgramArguments = [
+      "/usr/bin/open"
+      "-a"
+      "Raycast"
+    ];
+    RunAtLoad = true;
+  };
+
   # Enable Touch ID for sudo
   security.pam.services.sudo_local.touchIdAuth = true;
 
