c99611d6baa5c9157cac4df9a161416aa2b21f17
Etienne Robert <etiennerobert33@gmail.com>
2026-03-25 17:59:12 +0100

    nix: move host configs to separate module

---
+++ nix/hosts/default.nix
@@ -0,0 +1,80 @@
+{ inputs, ... }:
+let
+  inherit (inputs)
+    nixpkgs
+    nix-darwin
+    home-manager
+    agenix
+    neovim-nightly-overlay
+    pronto
+    ;
+
+  localPackagesOverlay = final: _prev: import ../pkgs { pkgs = final; };
+
+  mkHost =
+    {
+      builder,
+      agenixModule,
+      homeManagerModule,
+      homeModule,
+    }:
+    host:
+    builder {
+      specialArgs = { inherit pronto agenix; };
+      modules = [
+        ./${host}/configuration.nix
+        {
+          nixpkgs.overlays = [
+            neovim-nightly-overlay.overlays.default
+            localPackagesOverlay
+          ];
+        }
+        agenixModule
+        homeManagerModule
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.soft = import homeModule;
+          };
+        }
+      ];
+    };
+  mkNixosHost = mkHost {
+    builder = nixpkgs.lib.nixosSystem;
+    agenixModule = agenix.nixosModules.default;
+    homeManagerModule = home-manager.nixosModules.home-manager;
+    homeModule = ../modules/home/linux.nix;
+  };
+  mkDarwinHost = mkHost {
+    builder = nix-darwin.lib.darwinSystem;
+    agenixModule = agenix.darwinModules.default;
+    homeManagerModule = home-manager.darwinModules.home-manager;
+    homeModule = ../modules/home/darwin.nix;
+  };
+
+  inherit (nixpkgs.lib) genAttrs;
+
+  nixosHosts = [
+    "tower"
+    "leod"
+  ];
+
+  darwinHosts = [ "aaron" ];
+in
+{
+  flake = {
+
+    nixosConfigurations = genAttrs nixosHosts mkNixosHost // {
+      pi = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./pi/configuration.nix
+          agenix.nixosModules.default
+        ];
+      };
+    };
+
+    darwinConfigurations = genAttrs darwinHosts mkDarwinHost;
+  };
+}
