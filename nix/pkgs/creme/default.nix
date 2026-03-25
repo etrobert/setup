31f083d5d5685a73d02c1f9a04bd4b9219cb818f
Etienne Robert <etiennerobert33@gmail.com>
2026-03-25 22:34:56 +0100

    nix: inline creme script in default.nix, rm creme.sh

--- nix/pkgs/creme/default.nix
+++ nix/pkgs/creme/default.nix
@@ -3,5 +3,13 @@ writeShellApplication {
   name = "creme";
   runtimeInputs = [ mpc ];
   inheritPath = false;
-  text = builtins.readFile ./creme.sh;
+  text = ''
+    song=$(mpc current --format "%file%")
+    if [[ -z "$song" ]]; then
+      echo "Nothing is currently playing"
+      exit 1
+    fi
+    echo "$song" >>~/sync/playlists/creme.m3u
+    echo "Added: $song"
+  '';
 }
