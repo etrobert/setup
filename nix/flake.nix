05b50b2a5bc84a08fee2e7eb4f1d48a2102de540
Etienne Robert <etiennerobert33@gmail.com>
2026-03-25 18:06:43 +0100

    nix: remove outdated comments from flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -36,17 +36,13 @@
 
   outputs =
     inputs@{ home-manager, flake-parts, ... }:
-    # https://flake.parts/module-arguments.html
     flake-parts.lib.mkFlake { inherit inputs; } (
       { self, ... }:
       {
         imports = [
           ./hosts
-          # Optional: use external flake logic, e.g.
-          # inputs.foo.flakeModules.default
         ];
         flake = {
-          # Put your original flake attributes here.
           homeConfigurations =
             let
               mkHome =
@@ -81,7 +77,6 @@
 
         };
         systems = [
-          # systems for which you want to build the `perSystem` attributes
           "x86_64-linux"
           "aarch64-darwin"
         ];
@@ -115,14 +110,6 @@
             formatter = pkgs.nixfmt;
 
             packages = import ./pkgs { inherit pkgs; };
-
-            # Recommended: move all package definitions here.
-            # e.g. (assuming you have a nixpkgs input)
-            # packages.foo = pkgs.callPackage ./foo/package.nix { };
-            # packages.bar = pkgs.callPackage ./bar/package.nix {
-            #   foo = config.packages.foo;
-            # };
-
           };
       }
     );
