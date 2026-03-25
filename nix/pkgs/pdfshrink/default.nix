b5e2aa63ae68ebb9438d3e086f70e483c4bb6cb5
Etienne Robert <etiennerobert33@gmail.com>
2026-03-25 22:42:51 +0100

    nix: inline pdfshrink script, rm pdfshrink.sh

--- nix/pkgs/pdfshrink/default.nix
+++ nix/pkgs/pdfshrink/default.nix
@@ -3,5 +3,22 @@ writeShellApplication {
   name = "pdfshrink";
   runtimeInputs = [ ghostscript ];
   inheritPath = false;
-  text = builtins.readFile ./pdfshrink.sh;
+  text = ''
+    if [ "$#" -ne 1 ]; then
+      echo "Usage: pdfshrink <input.pdf>"
+      exit 1
+    fi
+
+    input="$1"
+    output="''${input%.pdf}-compressed.pdf"
+
+    gs -sDEVICE=pdfwrite \
+      -dCompatibilityLevel=1.4 \
+      -dPDFSETTINGS=/ebook \
+      -dNOPAUSE -dQUIET -dBATCH \
+      -sOutputFile="$output" \
+      "$input"
+
+    echo "Created $output"
+  '';
 }
