d835beec5bdf87ece2d91394aaa79c47e1a9b59e
Etienne Robert <etiennerobert33@gmail.com>
2026-03-25 15:40:34 +0100

    nix: manage .hushlogin with home.file attribute

--- nix/modules/home/darwin.nix
+++ nix/modules/home/darwin.nix
@@ -4,6 +4,8 @@
 
   home.homeDirectory = "/Users/${config.home.username}";
 
+  home.file.".hushlogin".text = "";
+
   home.shellAliases = {
     bg = "open /Volumes/T7/Applications/Baldur\'s\ Gate\ 3.app/Contents/MacOS/Baldur\'s\ Gate\ 3\ GOG";
   };
