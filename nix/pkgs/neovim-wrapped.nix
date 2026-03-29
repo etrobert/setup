bb558e4ca6773e666440ce1ed7c7745122213c4a
Etienne Robert <etiennerobert33@gmail.com>
2026-03-29 21:27:46 +0200

    neovim-wrapped: add pbcopy to PATH on darwin

--- nix/pkgs/neovim-wrapped.nix
+++ nix/pkgs/neovim-wrapped.nix
@@ -1,6 +1,8 @@
 {
+  stdenv,
   symlinkJoin,
   makeWrapper,
+  runCommand,
   neovim,
   lib,
   bash-language-server,
@@ -22,6 +24,14 @@
   typescript-language-server,
   vscode-langservers-extracted,
 }:
+let
+  pbcopy =
+    runCommand "pbcopy" { }
+      "
+    mkdir -p $out/bin
+    ln -s /usr/bin/pbcopy $out/bin/pbcopy
+  ";
+in
 symlinkJoin {
   name = "neovim-wrapped";
   buildInputs = [ makeWrapper ];
@@ -30,26 +40,30 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/nvim \
       --set PATH ${
-        lib.makeBinPath [
-          bash-language-server
-          black # python formatter
-          curl # used in my config
-          fd # used by telescope
-          gh # used by octo.lua
-          git
-          gopls
-          imagemagick # for image rendering in nvim using snacks.image
-          isort # python import sorter
-          lua-language-server
-          nixd
-          nixfmt
-          ripgrep # used by telescope
-          shfmt
-          stylua
-          tree-sitter
-          typescript-language-server
-          vscode-langservers-extracted
-        ]
+        lib.makeBinPath (
+          [
+            bash-language-server
+            black # python formatter
+            curl # used in my config
+            fd # used by telescope
+            gh # used by octo.lua
+            git
+            gopls
+            imagemagick # for image rendering in nvim using snacks.image
+            isort # python import sorter
+            lua-language-server
+            nixd
+            nixfmt
+            pbcopy
+            ripgrep # used by telescope
+            shfmt
+            stylua
+            tree-sitter
+            typescript-language-server
+            vscode-langservers-extracted
+          ]
+          ++ lib.optionals stdenv.isDarwin [ pbcopy ]
+        )
       }
   '';
 }
