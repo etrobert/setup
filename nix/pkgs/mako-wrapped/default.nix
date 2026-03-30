05541b6ebe6bff48732ae831648f9b7ae9afc16c
Etienne Robert <etiennerobert33@gmail.com>
2026-03-31 00:46:43 +0200

    mako-wrapped: inline config with writeText

--- nix/pkgs/mako-wrapped/default.nix
+++ nix/pkgs/mako-wrapped/default.nix
@@ -2,7 +2,23 @@
   symlinkJoin,
   makeWrapper,
   mako,
+  writeText,
 }:
+let
+  config = writeText "config" /* ini */ ''
+    font=monospace 12
+    margin=20
+    default-timeout=5000
+    border-radius=6
+    background-color=#24273a
+    border-color=#cdd6f4
+
+    # Firefox requests timeout=0 (never expire) for notifications like Google Calendar.
+    # Override this to use our default-timeout instead.
+    [app-name=Firefox]
+    ignore-timeout=1
+  '';
+in
 symlinkJoin {
   name = "mako-wrapped";
   nativeBuildInputs = [ makeWrapper ];
@@ -10,7 +26,7 @@ symlinkJoin {
   meta.mainProgram = "mako";
   postBuild = ''
     wrapProgram $out/bin/mako \
-      --add-flags "--config ${./config}"
+      --add-flags "--config ${config}"
 
     rm $out/share/dbus-1/services/fr.emersion.mako.service
     cp ${mako}/share/dbus-1/services/fr.emersion.mako.service \
