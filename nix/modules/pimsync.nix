00bfa4387a3c9041cd678aa80612bcc2fd913f67
Etienne Robert <etiennerobert33@gmail.com>
2026-03-30 18:20:23 +0200

    pimsync: inline config as nix file, remove static file

--- nix/modules/pimsync.nix
+++ nix/modules/pimsync.nix
@@ -1,6 +1,34 @@
 _: {
   flake.nixosModules.pimsync =
     { pkgs, lib, ... }:
+    let
+      configFile = pkgs.writeText "pimsync.conf" ''
+        status_path "~/.local/share/pimsync/status/"
+
+        storage contacts_icloud {
+          type carddav
+          url https://contacts.icloud.com
+          username etiennerobert33@gmail.com
+          interval 30
+          password {
+            cmd cat /run/agenix/apple-pimsync-password
+          }
+        }
+
+        storage contacts_local {
+          type vdir/vcard
+          path ~/.local/share/contacts/
+          fileext vcf
+          interval 30
+        }
+
+        pair contacts {
+          storage_a contacts_local
+          storage_b contacts_icloud
+          collections all
+        }
+      '';
+    in
     {
       age.secrets.apple-pimsync-password = {
         owner = "soft";
@@ -14,7 +42,7 @@ _: {
         wantedBy = [ "default.target" ];
         serviceConfig = {
           Type = "simple";
-          ExecStart = "${lib.getExe pkgs.pimsync} -v warn daemon";
+          ExecStart = "${lib.getExe pkgs.pimsync} -v warn -c ${configFile} daemon";
         };
       };
     };
