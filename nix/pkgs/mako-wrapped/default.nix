b4178fc3343b59e92889225fe2cd3de596711f15
Etienne Robert <etiennerobert33@gmail.com>
2026-03-30 22:02:18 +0200

    mako-wrapped: patch service files to wrapped binary

--- nix/pkgs/mako-wrapped/default.nix
+++ nix/pkgs/mako-wrapped/default.nix
@@ -11,5 +11,17 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/mako \
       --add-flags "--config ${./config}"
+
+    rm $out/share/dbus-1/services/fr.emersion.mako.service
+    cp ${mako}/share/dbus-1/services/fr.emersion.mako.service \
+      $out/share/dbus-1/services/fr.emersion.mako.service
+    substituteInPlace $out/share/dbus-1/services/fr.emersion.mako.service \
+      --replace-fail "${mako}/bin/mako" "$out/bin/mako"
+
+    rm $out/share/systemd/user/mako.service
+    cp ${mako}/share/systemd/user/mako.service \
+      $out/share/systemd/user/mako.service
+    substituteInPlace $out/share/systemd/user/mako.service \
+      --replace-fail "${mako}/bin/mako" "$out/bin/mako"
   '';
 }
