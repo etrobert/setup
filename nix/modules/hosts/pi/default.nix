33562a3faca6045169b33646481bbe32f22cf01a
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 20:24:20 +0200

    pi: add kanata-main overlay to nixpkgs.overlays

--- nix/modules/hosts/pi/default.nix
+++ nix/modules/hosts/pi/default.nix
@@ -5,6 +5,7 @@
     system = "aarch64-linux";
     modules = [
       ./configuration.nix
+      { nixpkgs.overlays = [ self.overlays.kanata-main ]; }
       self.nixosModules.nixosBase
       self.nixosModules.base
       inputs.agenix.nixosModules.default
