6c04dadb5e9838301eab9c686616c0ee43bd05ea
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 21:29:41 +0200

    switch: factor out Linux and Darwin implementations

--- nix/pkgs/switch.nix
+++ nix/pkgs/switch.nix
@@ -7,38 +7,42 @@
   nixos-rebuild,
   systemd,
 }:
-if stdenv.isLinux then
-  let
-    sudo = runCommandLocal "sudo" { } ''
-      mkdir -p $out/bin
-      ln -s /run/wrappers/bin/sudo $out/bin/sudo
-    '';
-  in
-  writeShellApplication {
-    name = "switch";
-    runtimeInputs = [
-      coreutils # for id
-      sudo
-      nixos-rebuild
-      systemd
-    ];
-    inheritPath = false;
-    text = "sudo nixos-rebuild switch";
-  }
-else
-  let
-    sudo = runCommandLocal "sudo" { } ''
-      mkdir -p $out/bin
-      ln -s /usr/bin/sudo $out/bin/sudo
-    '';
-  in
-  writeShellApplication {
-    name = "switch";
-    runtimeInputs = [
-      coreutils # for id
-      sudo
-      inputs'.nix-darwin.packages.darwin-rebuild
-    ];
-    inheritPath = false;
-    text = "sudo darwin-rebuild switch";
-  }
+let
+  switch-linux =
+    let
+      sudo = runCommandLocal "sudo" { } ''
+        mkdir -p $out/bin
+        ln -s /run/wrappers/bin/sudo $out/bin/sudo
+      '';
+    in
+    writeShellApplication {
+      name = "switch";
+      runtimeInputs = [
+        coreutils # for id
+        sudo
+        nixos-rebuild
+        systemd
+      ];
+      inheritPath = false;
+      text = "sudo nixos-rebuild switch";
+    };
+
+  switch-darwin =
+    let
+      sudo = runCommandLocal "sudo" { } ''
+        mkdir -p $out/bin
+        ln -s /usr/bin/sudo $out/bin/sudo
+      '';
+    in
+    writeShellApplication {
+      name = "switch";
+      runtimeInputs = [
+        coreutils # for id
+        sudo
+        inputs'.nix-darwin.packages.darwin-rebuild
+      ];
+      inheritPath = false;
+      text = "sudo darwin-rebuild switch";
+    };
+in
+if stdenv.isLinux then switch-linux else switch-darwin
