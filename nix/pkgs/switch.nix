b3c3e589f3b6f842c1f23cc06d018e1e61aa56e0
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 20:20:02 +0200

    switch: add systemd dependency on Linux

--- nix/pkgs/switch.nix
+++ nix/pkgs/switch.nix
@@ -6,6 +6,7 @@
   inputs',
   lib,
   nixos-rebuild,
+  systemd,
 }:
 let
   sudo-path = if stdenv.isDarwin then "/usr/bin/sudo" else "/run/wrappers/bin/sudo";
@@ -26,6 +27,7 @@ writeShellApplication {
   ]
   ++ lib.optionals stdenv.isLinux [
     nixos-rebuild
+    systemd
   ];
   inheritPath = false;
   text = if stdenv.isLinux then "sudo nixos-rebuild switch" else "sudo darwin-rebuild switch";
