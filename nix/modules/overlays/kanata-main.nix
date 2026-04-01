0622cef25b97190f85fe400ddd64e365b6d187aa
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 18:53:58 +0200

    kanata-main: flatten overlay function for clarity

--- nix/modules/overlays/kanata-main.nix
+++ nix/modules/overlays/kanata-main.nix
@@ -1,19 +1,17 @@
 _: {
-  flake.overlays.kanata-main = (
-    final: prev: {
-      kanata = prev.kanata.overrideAttrs (_: rec {
-        doCheck = false;
-        version = "main";
-        src = final.fetchFromGitHub {
-          owner = "jtroo";
-          repo = "kanata";
-          rev = "484368f406584255208dfd59359130f3769baf52";
-          hash = "sha256-IXnYds2pHLS0dOh2vDSP/0bA/8YmCuprJXAOgI0TDn4=";
-        };
-        cargoDeps = prev.rustPlatform.importCargoLock {
-          lockFile = "${src}/Cargo.lock";
-        };
-      });
-    }
-  );
+  flake.overlays.kanata-main = final: prev: {
+    kanata = prev.kanata.overrideAttrs (_: rec {
+      doCheck = false;
+      version = "main";
+      src = final.fetchFromGitHub {
+        owner = "jtroo";
+        repo = "kanata";
+        rev = "484368f406584255208dfd59359130f3769baf52";
+        hash = "sha256-IXnYds2pHLS0dOh2vDSP/0bA/8YmCuprJXAOgI0TDn4=";
+      };
+      cargoDeps = prev.rustPlatform.importCargoLock {
+        lockFile = "${src}/Cargo.lock";
+      };
+    });
+  };
 }
