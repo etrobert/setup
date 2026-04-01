1eb2e3c69e2c8dd35a48450ac25fb224146aa974
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 18:50:35 +0200

    leod: add kanata-main overlay to nixpkgs.overlays

--- nix/modules/hosts/leod/default.nix
+++ nix/modules/hosts/leod/default.nix
@@ -17,6 +17,7 @@ in
     specialArgs = { inherit self pronto agenix; };
     modules = [
       ./configuration.nix
+      { nixpkgs.overlays = [ self.overlays.kanata-main ]; }
       nix-index-database.nixosModules.default
       home-manager.nixosModules.home-manager
       agenix.nixosModules.default
