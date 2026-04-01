f850da7dde8dfb5f3749859cfe8bd79d1a80e963
Etienne Robert <etiennerobert33@gmail.com>
2026-04-01 21:02:17 +0200

    kanata: inline overlay, remove kanata-main overlay file

--- nix/modules/hosts/pi/default.nix
+++ nix/modules/hosts/pi/default.nix
@@ -5,7 +5,6 @@
     system = "aarch64-linux";
     modules = [
       ./configuration.nix
-      { nixpkgs.overlays = [ self.overlays.kanata-main ]; }
       self.nixosModules.nixosBase
       self.nixosModules.base
       inputs.agenix.nixosModules.default
--- nix/modules/nixos-base.nix
+++ nix/modules/nixos-base.nix
@@ -20,6 +20,19 @@ _: {
       services = {
         kanata = {
           enable = true;
+          package = pkgs.kanata.overrideAttrs (_: rec {
+            doCheck = false;
+            version = "main";
+            src = pkgs.fetchFromGitHub {
+              owner = "jtroo";
+              repo = "kanata";
+              rev = "484368f406584255208dfd59359130f3769baf52";
+              hash = "sha256-IXnYds2pHLS0dOh2vDSP/0bA/8YmCuprJXAOgI0TDn4=";
+            };
+            cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = "${src}/Cargo.lock";
+            };
+          });
           keyboards.default = {
             config = /* scheme */ ''
               (defsrc caps a s d f j k l ;)
