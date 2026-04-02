ae93fc24288417a32c4785ed89fd8c4c84048b8d
Etienne Robert <etiennerobert33@gmail.com>
2026-04-02 13:46:22 +0200

    niri-wrapped: patch systemd service to use wrapped niri

--- nix/pkgs/niri-wrapped/default.nix
+++ nix/pkgs/niri-wrapped/default.nix
@@ -10,6 +10,12 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/niri \
       --set NIRI_CONFIG ${./config.kdl}
+
+    rm $out/share/systemd/user/niri.service
+    cp ${niri}/share/systemd/user/niri.service \
+      $out/share/systemd/user/niri.service
+    substituteInPlace $out/share/systemd/user/niri.service \
+      --replace-fail "${niri}/bin/niri" "$out/bin/niri"
   '';
   passthru.providedSessions = niri.passthru.providedSessions;
 }
