3b776301ddeaf577fddc6d7270427cfdb987a428
Etienne Robert <etiennerobert33@gmail.com>
2026-04-06 01:20:34 +0200

    spider: move keymaps into plugin config directory

--- nix/pkgs/neovim-wrapped/plugins/spider/default.nix
+++ nix/pkgs/neovim-wrapped/plugins/spider/default.nix
@@ -3,7 +3,13 @@
   plugins = [
     {
       plugin = pkgs.vimPlugins.nvim-spider;
-      luaConfig = /* lua */ ''require("spider").setup({ skipInsignificantPunctuation = false })'';
+      luaConfig = /* lua */ ''
+        require("spider").setup({ skipInsignificantPunctuation = false })
+
+        vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
+        vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
+        vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
+      '';
     }
   ];
 }
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -21,10 +21,6 @@ require("which-key").setup({
 	},
 })
 
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
-vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
-vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
-
 require("plugins.harpoon")
 require("plugins.conform")
 require("plugins.gitsigns")
