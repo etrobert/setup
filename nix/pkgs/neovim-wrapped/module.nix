2df6da2200c63a44f25802b3bb888ef4d614679e
Etienne Robert <etiennerobert33@gmail.com>
2026-04-06 00:24:43 +0200

    neovim-wrapped: add luaConfig option for plugins

--- nix/pkgs/neovim-wrapped/module.nix
+++ nix/pkgs/neovim-wrapped/module.nix
@@ -5,19 +5,31 @@ in
 {
   options.plugins = mkOption {
     type = types.listOf (
-      types.submodule {
-        options = {
-          plugin = mkOption { type = types.package; };
-          config = mkOption {
-            type = types.nullOr types.lines;
-            default = null;
+      types.submodule (
+        { config, ... }:
+        {
+          options = {
+            plugin = mkOption { type = types.package; };
+            luaConfig = mkOption {
+              type = types.nullOr types.lines;
+              default = null;
+            };
+            config = mkOption {
+              type = types.nullOr types.lines;
+              default = null;
+            };
+            extraPackages = lib.mkOption {
+              type = lib.types.listOf lib.types.package;
+              default = [ ];
+            };
           };
-          extraPackages = lib.mkOption {
-            type = lib.types.listOf lib.types.package;
-            default = [ ];
-          };
-        };
-      }
+          config.config = lib.mkIf (config.luaConfig != null) /* vim */ ''
+            lua << EOF
+            ${config.luaConfig}
+            EOF
+          '';
+        }
+      )
     );
     default = [ ];
   };
--- nix/pkgs/neovim-wrapped/plugins/lualine-nvim.nix
+++ nix/pkgs/neovim-wrapped/plugins/lualine-nvim.nix
@@ -3,14 +3,12 @@
   plugins = [
     {
       plugin = vimPlugins.lualine-nvim;
-      config = /* vim */ ''
-        lua << EOF
-          local relative_path = { "filename", path = 1 }
-          require("lualine").setup({
-          	sections = { lualine_c = { relative_path }, lualine_x = { "filetype" } },
-          	inactive_sections = { lualine_c = { relative_path } },
-          })
-        EOF
+      luaConfig = /* lua */ ''
+        local relative_path = { "filename", path = 1 }
+        require("lualine").setup({
+        	sections = { lualine_c = { relative_path }, lualine_x = { "filetype" } },
+        	inactive_sections = { lualine_c = { relative_path } },
+        })
       '';
     }
   ];
