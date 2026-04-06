2db282fa5a34e37dcd9b7ab34a8508ce9b4180df
Etienne Robert <etiennerobert33@gmail.com>
2026-04-06 13:53:52 +0200

    gitsigns: fix extraPackages indentation

--- nix/pkgs/neovim-wrapped/plugins/gitsigns/default.nix
+++ nix/pkgs/neovim-wrapped/plugins/gitsigns/default.nix
@@ -9,7 +9,7 @@
     {
       plugin = pkgs.vimPlugins.gitsigns-nvim;
       luaConfig = builtins.readFile ./config.lua;
-      extraPackages = (if with-git-wrapped then [ self'.packages.git-wrapped ] else with pkgs; [ git ]);
+      extraPackages = if with-git-wrapped then [ self'.packages.git-wrapped ] else with pkgs; [ git ];
     }
   ];
 }
