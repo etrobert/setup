60d72f7e76ee30c2c522aa4845a74046cbb04916
Etienne Robert <etiennerobert33@gmail.com>
2026-04-06 17:06:54 +0200

    Revert "init.lua: delete all installed plugins on startup"
    This reverts commit 76fc30ca699684acbd8d91c148b787353ac577a1.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,14 +3,6 @@ vim.g.start_time = vim.fn.reltime()
 
 vim.loader.enable()
 
--- TODO: Remove, this deletes all installed plugins
-local all = vim.iter(vim.pack.get())
-	:map(function(x)
-		return x.spec.name
-	end)
-	:totable()
-vim.pack.del(all)
-
 require("etrobert")
 
 vim.cmd.packadd("nvim.undotree")
