d011f6bfdd7d2a99f012e69cf51d4d567a10e713
Etienne Robert <etiennerobert33@gmail.com>
2026-04-07 19:32:11 +0200

    neovim: detect graphical session via COLORTERM variable

--- nix/pkgs/neovim-wrapped/plugins/catppuccin/default.nix
+++ nix/pkgs/neovim-wrapped/plugins/catppuccin/default.nix
@@ -4,8 +4,8 @@
     {
       plugin = pkgs.vimPlugins.catppuccin-nvim;
       luaConfig = /* lua */ ''
-        if os.getenv("WAYLAND_DISPLAY") or vim.fn.has("mac") == 1 then
-        	-- Graphical session (Wayland on Linux or macOS)
+        if os.getenv("COLORTERM") == "truecolor" then
+        	-- Graphical session
         	require("catppuccin").setup({ float = { transparent = true, solid = false } })
         	vim.cmd("colorscheme catppuccin-macchiato")
         	-- else we're in a tty, using default theme
