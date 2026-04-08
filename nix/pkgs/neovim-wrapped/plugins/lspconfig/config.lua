5e0f30000a3c106b31e6989099c563d42b57a992
Etienne Robert <etiennerobert33@gmail.com>
2026-04-08 22:58:46 +0200

    neovim: move lspconfig config to separate lua file

---
+++ nix/pkgs/neovim-wrapped/plugins/lspconfig/config.lua
@@ -0,0 +1,83 @@
+vim.lsp.config("gopls", {
+	settings = {
+		gopls = {
+			codelenses = {
+				gc_details = true,
+				generate = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+			},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+		},
+	},
+})
+
+local ts_ls_opts = {
+	inlayHints = {
+		-- Show argument labels at call sites: foo(/*timeout:*/ 1000). "all" shows for every arg, "literals" only for literal values.
+		includeInlayParameterNameHints = "all",
+		-- Suppress the above hint when the variable name already matches the parameter name: foo(timeout) needs no label.
+		includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+		-- Show types of parameters in function signatures: (x: number, y: string).
+		includeInlayFunctionParameterTypeHints = true,
+		-- Show inferred types of const/let variables: const x: number = ...
+		includeInlayVariableTypeHints = true,
+		-- Suppress the above hint when the variable name already matches the type name: const user: User is redundant.
+		includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+		-- Show inferred types on class property declarations: name: string.
+		includeInlayPropertyDeclarationTypeHints = true,
+		-- Show inferred return types on functions when not explicitly annotated: function foo(): string.
+		includeInlayFunctionLikeReturnTypeHints = true,
+		-- Show numeric values of enum members: Red = 0, Green = 1.
+		includeInlayEnumMemberValueHints = true,
+	},
+}
+
+vim.lsp.config("ts_ls", {
+	settings = {
+		typescript = ts_ls_opts,
+		javascript = ts_ls_opts,
+	},
+})
+
+local inlay_hint_max_length = 40
+
+local orig_inlay_hint_handler = vim.lsp.handlers["textDocument/inlayHint"]
+-- We override the inlay hint handler to filter out hints that are too long
+vim.lsp.handlers["textDocument/inlayHint"] = function(err, result, ctx, config)
+	if result then
+		local client = vim.lsp.get_client_by_id(ctx.client_id)
+		if client and client.name == "ts_ls" then
+			result = vim.tbl_filter(function(hint)
+				local text = table.concat(vim.tbl_map(function(p)
+					return p.value
+				end, hint.label))
+				return #text <= inlay_hint_max_length
+			end, result)
+		end
+	end
+	orig_inlay_hint_handler(err, result, ctx, config)
+end
+
+vim.lsp.enable({
+	"bashls",
+	"eslint",
+	"tailwindcss",
+	"ts_ls",
+	"rust_analyzer",
+	"gopls",
+	"lua_ls",
+	"nixd",
+	"cssls",
+	"html",
+	-- "copilot",
+})
--- nix/pkgs/neovim-wrapped/plugins/lspconfig/default.nix
+++ nix/pkgs/neovim-wrapped/plugins/lspconfig/default.nix
@@ -4,73 +4,7 @@
     {
       plugin = pkgs.vimPlugins.nvim-lspconfig;
 
-      luaConfig = /* lua */ ''
-        vim.lsp.config("gopls", {
-        	settings = {
-        		gopls = {
-        			codelenses = {
-        				gc_details = true,
-        				generate = true,
-        				test = true,
-        				tidy = true,
-        				upgrade_dependency = true,
-        			},
-        			hints = {
-        				assignVariableTypes = true,
-        				compositeLiteralFields = true,
-        				compositeLiteralTypes = true,
-        				constantValues = true,
-        				functionTypeParameters = true,
-        				parameterNames = true,
-        				rangeVariableTypes = true,
-        			},
-        		},
-        	},
-        })
-
-        vim.lsp.config("ts_ls", {
-        	settings = {
-        		typescript = {
-        			inlayHints = {
-        				includeInlayParameterNameHints = "all",
-        				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        				includeInlayFunctionParameterTypeHints = true,
-        				includeInlayVariableTypeHints = true,
-        				includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        				includeInlayPropertyDeclarationTypeHints = true,
-        				includeInlayFunctionLikeReturnTypeHints = true,
-        				includeInlayEnumMemberValueHints = true,
-        			},
-        		},
-        		javascript = {
-        			inlayHints = {
-        				includeInlayParameterNameHints = "all",
-        				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        				includeInlayFunctionParameterTypeHints = true,
-        				includeInlayVariableTypeHints = true,
-        				includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        				includeInlayPropertyDeclarationTypeHints = true,
-        				includeInlayFunctionLikeReturnTypeHints = true,
-        				includeInlayEnumMemberValueHints = true,
-        			},
-        		},
-        	},
-        })
-
-        vim.lsp.enable({
-        	"bashls",
-        	"eslint",
-        	"tailwindcss",
-        	"ts_ls",
-        	"rust_analyzer",
-        	"gopls",
-        	"lua_ls",
-        	"nixd",
-        	"cssls",
-        	"html",
-        	-- "copilot",
-        })
-      '';
+      luaConfig = builtins.readFile ./config.lua;
 
       extraPackages = with pkgs; [
         bash-language-server
