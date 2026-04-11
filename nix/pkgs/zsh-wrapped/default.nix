115a3f78fd0787a5f7d6a651c3ce665ba8800432
Etienne Robert <etiennerobert33@gmail.com>
2026-04-11 21:23:00 +0200

    zsh-wrapped: enhance .zshrc and add autosuggestions

--- nix/pkgs/zsh-wrapped/default.nix
+++ nix/pkgs/zsh-wrapped/default.nix
@@ -3,7 +3,22 @@
   symlinkJoin,
   makeWrapper,
   zsh,
+  writeText,
+  linkFarm,
+  zsh-autosuggestions,
 }:
+let
+  zshrcFinal = writeText "zshrc" ''
+    source ${./config/.zshrc}
+    source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+  '';
+  zdotdir = linkFarm "zdotdir" [
+    {
+      name = ".zshrc";
+      path = zshrcFinal;
+    }
+  ];
+in
 symlinkJoin {
   name = "zsh-wrapped";
   nativeBuildInputs = [ makeWrapper ];
@@ -11,7 +26,7 @@ symlinkJoin {
   meta.mainProgram = "zsh";
   postBuild = ''
     wrapProgram $out/bin/zsh \
-      --set ZDOTDIR ${./config} \
+      --set ZDOTDIR ${zdotdir} \
       --prefix PATH : ${lib.makeBinPath [ ]}
   '';
 }
