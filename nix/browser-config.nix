0e6cf010af1ce31e437f4893aa4a4c6b3d033570
Etienne Robert <etiennerobert33@gmail.com>
2026-04-13 10:40:31 +0200

    refactor browser prefs & policies to shared config module

---
+++ nix/browser-config.nix
@@ -0,0 +1,50 @@
+let
+  renderPrefValue =
+    value:
+    if builtins.isBool value then
+      if value then "true" else "false"
+    else if builtins.isInt value then
+      toString value
+    else
+      builtins.toJSON value;
+
+  renderDefaultPrefs =
+    settings:
+    builtins.concatStringsSep "\n" (
+      map (name: "defaultPref(${builtins.toJSON name}, ${renderPrefValue settings.${name}});") (
+        builtins.attrNames settings
+      )
+    );
+
+  sharedPolicies = {
+    PasswordManagerEnabled = false;
+    SearchEngines = {
+      Default = "DuckDuckGo";
+    };
+    ExtensionSettings = {
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+        default_area = "menupanel";
+      };
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        installation_mode = "force_installed";
+        default_area = "navbar";
+      };
+      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+        installation_mode = "force_installed";
+        default_area = "menupanel";
+      };
+    };
+  };
+
+  sharedSettings = {
+    "browser.ctrlTab.sortByRecentlyUsed" = true;
+    "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = true;
+  };
+in
+{
+  inherit renderDefaultPrefs sharedPolicies sharedSettings;
+}
--- nix/pkgs/zen-browser-wrapped/default.nix
+++ nix/pkgs/zen-browser-wrapped/default.nix
@@ -1,42 +1,18 @@
 { inputs', wrapFirefox }:
+let
+  browserConfig = import ../../browser-config.nix;
+in
 wrapFirefox inputs'.zen-browser.packages.zen-browser-unwrapped {
   extraPrefs = /* javascript */ ''
-    defaultPref("browser.ctrlTab.sortByRecentlyUsed", true);
-    defaultPref(
-      "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled",
-      true,
-    );
-
-    defaultPref("zen.theme.content-element-separation", 4);
-    defaultPref("zen.theme.border-radius", 12);
+    ${browserConfig.renderDefaultPrefs (
+      browserConfig.sharedSettings
+      // {
+        "zen.theme.content-element-separation" = 4;
+        "zen.theme.border-radius" = 12;
+      }
+    )}
   '';
-  extraPolicies = {
-    PasswordManagerEnabled = false;
-
+  extraPolicies = browserConfig.sharedPolicies // {
     DontCheckDefaultBrowser = true;
-
-    SearchEngines = {
-      Default = "DuckDuckGo";
-    };
-
-    ExtensionSettings = {
-      "uBlock0@raymondhill.net" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        installation_mode = "force_installed";
-        default_area = "menupanel";
-      };
-
-      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-        installation_mode = "force_installed";
-        default_area = "navbar";
-      };
-
-      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
-        installation_mode = "force_installed";
-        default_area = "menupanel";
-      };
-    };
   };
 }
