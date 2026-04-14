16c71ba0308f7e65ac322aecb8e6dbf0b7f25b72
Etienne Robert <etiennerobert33@gmail.com>
2026-04-14 15:03:52 +0200

    set zen as default handler for pdf mime type

--- nix/modules/nixos-workstation.nix
+++ nix/modules/nixos-workstation.nix
@@ -138,6 +138,7 @@ _: {
           x-scheme-handler/https=zen.desktop
           x-scheme-handler/about=zen.desktop
           x-scheme-handler/unknown=zen.desktop
+          application/pdf=zen.desktop
         '';
       };
 
