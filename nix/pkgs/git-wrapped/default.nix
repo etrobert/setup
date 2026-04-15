f33ead89e60f4eb01cb615324e588499d701ab79
Etienne Robert <etiennerobert33@gmail.com>
2026-04-15 11:30:46 +0200

    refactor git-wrapped to use pkgs and deps let binding

--- nix/pkgs/git-wrapped/default.nix
+++ nix/pkgs/git-wrapped/default.nix
@@ -1,31 +1,28 @@
 {
+  pkgs,
   # self',
   lib,
-  symlinkJoin,
-  makeWrapper,
-  git,
-  difftastic,
-  fzf,
   userConfig ? ./gitconfig-user,
 }:
 # TODO: --set PATH
-symlinkJoin {
+let
+  deps = with pkgs; [
+    # TODO: Fix this
+    # Removed so that neovim-wrapped is not included on the pi
+    # self'.packages.gen-commit-msg
+    difftastic
+    fzf
+  ];
+in
+pkgs.symlinkJoin {
   name = "git-wrapped";
-  nativeBuildInputs = [ makeWrapper ];
-  paths = [ git ];
+  nativeBuildInputs = with pkgs; [ makeWrapper ];
+  paths = with pkgs; [ git ];
   meta.mainProgram = "git";
   postBuild = ''
     wrapProgram $out/bin/git \
       --set GIT_CONFIG_SYSTEM ${./gitconfig-system} \
       --set GIT_CONFIG_GLOBAL ${userConfig} \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          # TODO: Fix this
-          # Removed so that neovim-wrapped is not included on the pi
-          # self'.packages.gen-commit-msg
-          difftastic
-          fzf
-        ]
-      }
-'';
+      --prefix PATH : ${lib.makeBinPath deps}
+  '';
 }
