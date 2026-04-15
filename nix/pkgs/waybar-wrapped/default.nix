5f06390a3afc9aaaae3bcc691c696bb604de5b04
Etienne Robert <etiennerobert33@gmail.com>
2026-04-15 11:02:36 +0200

    refactor waybar-wrapped to use pkgs and with scope

--- nix/pkgs/waybar-wrapped/default.nix
+++ nix/pkgs/waybar-wrapped/default.nix
@@ -1,20 +1,14 @@
 {
   self',
-  symlinkJoin,
-  makeWrapper,
   lib,
-  waybar,
-  jq,
-  coreutils,
-  gawk,
-  pavucontrol,
+  pkgs,
   dev ? false,
 }:
 let
   path = "/home/soft/setup/nix/pkgs/waybar-wrapped";
   config = if dev then path + "/config.jsonc" else ./config.jsonc;
   style = if dev then path + "/style.css" else ./style.css;
-  runtimeDeps = lib.makeBinPath [
+  runtimeDeps = with pkgs; [
     coreutils
     self'.packages.get-weather
     jq # used by custom/weekday and custom/cpu-governor
@@ -22,15 +16,15 @@ let
     pavucontrol
   ];
 in
-symlinkJoin {
+pkgs.symlinkJoin {
   name = "waybar-wrapped";
-  nativeBuildInputs = [ makeWrapper ];
-  paths = [ waybar ];
+  nativeBuildInputs = with pkgs; [ makeWrapper ];
+  paths = with pkgs; [ waybar ];
   meta.mainProgram = "waybar";
   postBuild = ''
     wrapProgram $out/bin/waybar \
       --add-flags "--config ${config}" \
       --add-flags "--style ${style}" \
-      --set PATH ${runtimeDeps}
+      --set PATH ${lib.makeBinPath runtimeDeps}
   '';
 }
