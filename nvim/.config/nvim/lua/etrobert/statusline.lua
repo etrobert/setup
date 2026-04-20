fd0c10794975cc90cda73c6feb05c71ae03d1b2a
Etienne Robert <etiennerobert33@gmail.com>
2026-04-20 11:20:57 +0200

    show filetype icon and color in statusline

--- nvim/.config/nvim/lua/etrobert/statusline.lua
+++ nvim/.config/nvim/lua/etrobert/statusline.lua
@@ -1,24 +1,24 @@
 local p = require("catppuccin.palettes").get_palette("macchiato")
-vim.api.nvim_set_hl(0, "StatuslineModeNormal",      { fg = p.base, bg = p.blue,  bold = true })
-vim.api.nvim_set_hl(0, "StatuslineModeNormalSep",   { fg = p.blue })
-vim.api.nvim_set_hl(0, "StatuslineModeInsert",      { fg = p.base, bg = p.green, bold = true })
-vim.api.nvim_set_hl(0, "StatuslineModeInsertSep",   { fg = p.green })
-vim.api.nvim_set_hl(0, "StatuslineModeVisual",      { fg = p.base, bg = p.mauve, bold = true })
-vim.api.nvim_set_hl(0, "StatuslineModeVisualSep",   { fg = p.mauve })
-vim.api.nvim_set_hl(0, "StatuslineModeCommand",     { fg = p.base, bg = p.red,   bold = true })
-vim.api.nvim_set_hl(0, "StatuslineModeCommandSep",  { fg = p.red })
-vim.api.nvim_set_hl(0, "StatuslineModeTerminal",    { fg = p.base, bg = p.teal,  bold = true })
+vim.api.nvim_set_hl(0, "StatuslineModeNormal", { fg = p.base, bg = p.blue, bold = true })
+vim.api.nvim_set_hl(0, "StatuslineModeNormalSep", { fg = p.blue })
+vim.api.nvim_set_hl(0, "StatuslineModeInsert", { fg = p.base, bg = p.green, bold = true })
+vim.api.nvim_set_hl(0, "StatuslineModeInsertSep", { fg = p.green })
+vim.api.nvim_set_hl(0, "StatuslineModeVisual", { fg = p.base, bg = p.mauve, bold = true })
+vim.api.nvim_set_hl(0, "StatuslineModeVisualSep", { fg = p.mauve })
+vim.api.nvim_set_hl(0, "StatuslineModeCommand", { fg = p.base, bg = p.red, bold = true })
+vim.api.nvim_set_hl(0, "StatuslineModeCommandSep", { fg = p.red })
+vim.api.nvim_set_hl(0, "StatuslineModeTerminal", { fg = p.base, bg = p.teal, bold = true })
 vim.api.nvim_set_hl(0, "StatuslineModeTerminalSep", { fg = p.teal })
 
 local mode_hls = {
-	n  = "StatuslineModeNormal",
-	i  = "StatuslineModeInsert",
+	n = "StatuslineModeNormal",
+	i = "StatuslineModeInsert",
 	ic = "StatuslineModeInsert",
-	v  = "StatuslineModeVisual",
-	V  = "StatuslineModeVisual",
+	v = "StatuslineModeVisual",
+	V = "StatuslineModeVisual",
 	["\22"] = "StatuslineModeVisual",
-	c  = "StatuslineModeCommand",
-	t  = "StatuslineModeTerminal",
+	c = "StatuslineModeCommand",
+	t = "StatuslineModeTerminal",
 }
 
 local sep = "\u{E0B0}"
@@ -27,6 +27,12 @@ local function mode_section()
 	local m = vim.fn.mode()
 	local hl = mode_hls[m] or "StatusLine"
 	return "%#" .. hl .. "# " .. m .. " %#" .. hl .. "Sep#" .. sep .. "%*"
+end
+
+local function filetype_section()
+	local ft = vim.bo.filetype
+	local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(ft, { default = true })
+	return "%#" .. hl .. "#" .. icon .. "%* %y"
 end
 
 vim.opt.statusline = "%!v:lua.require'etrobert.statusline'.render()"
@@ -42,7 +48,9 @@ return {
 
 		return table.concat({
 			mode_section(),
-			" %f %= %y %l:%c",
+			" %f %= ",
+			filetype_section(),
+			" %l:%c",
 		})
 	end,
 }
