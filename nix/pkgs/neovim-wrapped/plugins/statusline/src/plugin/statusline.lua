92000b6a0031f9b464b30334ebe504173088c6fc
Etienne Robert <etiennerobert33@gmail.com>
2026-04-20 21:55:10 +0200

    redraw statusline on ModeChanged autocmd event

--- nix/pkgs/neovim-wrapped/plugins/statusline/src/plugin/statusline.lua
+++ nix/pkgs/neovim-wrapped/plugins/statusline/src/plugin/statusline.lua
@@ -1,2 +1,8 @@
 vim.opt.showmode = false
 vim.opt.statusline = "%!v:lua.require('statusline').render()"
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	callback = function()
+		vim.cmd.redrawstatus()
+	end,
+})
