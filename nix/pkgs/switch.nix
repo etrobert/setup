28eacf894112157c2c14e2763d21796d8dfa718b
Etienne Robert <etiennerobert33@gmail.com>
2026-04-21 00:08:36 +0200

    refactor switch: handle linux and darwin in one expr

--- nix/pkgs/switch.nix
+++ nix/pkgs/switch.nix
@@ -13,30 +13,18 @@ let
       ln -s ${path} $out/bin/sudo
     '';
 
-  switch-linux = pkgs.writeShellApplication {
-    name = "switch";
-    # nh calls `sudo env nixos-rebuild ...`; all three must be in PATH so nh
-    # can resolve them to absolute store paths before invoking sudo
-    runtimeInputs = with pkgs; [
-      coreutils
-      nh
-      nix
-      sudo
-    ];
-    inheritPath = false;
-    text = "nh os switch";
-  };
-
-  switch-darwin = pkgs.writeShellApplication {
-    name = "switch";
-    runtimeInputs = with pkgs; [
-      coreutils
-      nh
-      nix
-      sudo
-    ];
-    inheritPath = false;
-    text = "nh darwin switch";
-  };
+  nhSubcommand = if pkgs.stdenv.isLinux then "os" else "darwin";
 in
-if pkgs.stdenv.isLinux then switch-linux else switch-darwin
+pkgs.writeShellApplication {
+  name = "switch";
+  # nh calls `sudo env nixos-rebuild ...`; all three must be in PATH so nh
+  # can resolve them to absolute store paths before invoking sudo
+  runtimeInputs = with pkgs; [
+    coreutils
+    nh
+    nix
+    sudo
+  ];
+  inheritPath = false;
+  text = "nh ${nhSubcommand} switch";
+}
