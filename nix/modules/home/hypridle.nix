634b6cf8476c11bb0cd5b18a21dc9362e2e39d9a
Etienne Robert <etiennerobert33@gmail.com>
2026-04-28 16:04:17 +0200

    flatten hypridle module without extra lambda nesting

--- nix/modules/home/hypridle.nix
+++ nix/modules/home/hypridle.nix
@@ -1,25 +1,23 @@
 _: {
-  flake.homeModules.hypridle =
-    { ... }:
-    {
-      services.hypridle = {
-        enable = true;
-        settings = {
-          general = {
-            lock_cmd = "pidof hyprlock || hyprlock";
-            before_sleep_cmd = "loginctl lock-session";
-          };
-          listener = [
-            {
-              timeout = 300; # 5 minutes
-              on-timeout = "loginctl lock-session";
-            }
-            {
-              timeout = 900; # 15 minutes
-              on-timeout = "systemctl suspend";
-            }
-          ];
+  flake.homeModules.hypridle = {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
         };
+        listener = [
+          {
+            timeout = 300; # 5 minutes
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 900; # 15 minutes
+            on-timeout = "systemctl suspend";
+          }
+        ];
       };
     };
+  };
 }
