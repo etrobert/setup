de734fb5f7bcc703c3cf6d79ce577c015ec177ea
Etienne Robert <etiennerobert33@gmail.com>
2026-05-04 18:27:22 +0200

    feat: enable ollama with qwen3:14b on tower host

--- nix/modules/hosts/tower/configuration.nix
+++ nix/modules/hosts/tower/configuration.nix
@@ -1,7 +1,7 @@
 # Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
 
@@ -23,4 +23,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
+  services.ollama = {
+    enable = true;
+    loadModels = [ "qwen3:14b" ];
+    syncModels = true;
+  };
 }
