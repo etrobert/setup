da5d004763c5917a6c37073d3aecd6892f4a38d5
Etienne Robert <etiennerobert33@gmail.com>
2026-05-08 22:17:08 +0200

    feat: bundle telescope-extras scripts as plugin

--- nix/pkgs/neovim-wrapped/plugins/telescope/default.nix
+++ nix/pkgs/neovim-wrapped/plugins/telescope/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
+let
+  telescope-extras = pkgs.vimUtils.buildVimPlugin {
+    name = "telescope-extras";
+    src = ./telescope-extras;
+    dependencies = [ pkgs.vimPlugins.telescope-nvim pkgs.vimPlugins.plenary-nvim ];
+  };
+in
 {
   plugins = [
     {
@@ -12,5 +19,6 @@
     { plugin = pkgs.vimPlugins.telescope-ui-select-nvim; }
     { plugin = pkgs.vimPlugins.telescope-fzf-native-nvim; }
     { plugin = pkgs.vimPlugins.plenary-nvim; }
+    { plugin = telescope-extras; }
   ];
 }
