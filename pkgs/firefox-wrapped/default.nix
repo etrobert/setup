b388fe969798c44d15ec221115817f3c0ee36deb
Etienne Robert <etiennerobert33@gmail.com>
2026-05-11 21:39:03 +0200

    simplify firefox extraPrefs assignment

--- pkgs/firefox-wrapped/default.nix
+++ pkgs/firefox-wrapped/default.nix
@@ -8,8 +8,6 @@ let
   browserConfig = import (self + /lib/browser-config.nix) { inherit lib; };
 in
 wrapFirefox firefox-unwrapped {
-  extraPrefs = ''
-    ${browserConfig.renderDefaultPrefs browserConfig.sharedSettings}
-  '';
+  extraPrefs = browserConfig.renderDefaultPrefs browserConfig.sharedSettings;
   extraPolicies = browserConfig.sharedPolicies;
 }
