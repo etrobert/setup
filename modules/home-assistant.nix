b355569dbb42763feb4442fd381b97debc19ff5d
Etienne Robert <etiennerobert33@gmail.com>
2026-05-21 17:37:49 +0200

    home-assistant: trust 127.0.0.1 network (#108)

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -17,6 +17,7 @@ _: {
           {
             type = "trusted_networks";
             trusted_networks = [
+              "127.0.0.1/32"
               "192.168.0.0/24"
               "100.64.0.0/10"
             ];
