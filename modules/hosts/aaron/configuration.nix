441021f4eb902232d12043ece9d1223352f8cfbb
Etienne Robert <etiennerobert33@gmail.com>
2026-09-06 16:54:53 +0200

    refactor(aaron): inline darwin home module in host configuration

--- modules/hosts/aaron/configuration.nix
+++ modules/hosts/aaron/configuration.nix
@@ -222,7 +222,30 @@ _: {
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
-        users.soft = self.homeModules.darwin;
+        users.soft =
+          { config, ... }:
+          {
+            home = {
+              username = "soft";
+              homeDirectory = "/Users/${config.home.username}";
+
+              # This value determines the Home Manager release that your
+              # configuration is compatible with. This helps avoid breakage
+              # when a new Home Manager release introduces backwards
+              # incompatible changes.
+              #
+              # You can update Home Manager without changing this value. See
+              # the Home Manager release notes for a list of state version
+              # changes in each release.
+              stateVersion = "25.11";
+            };
+
+            services.syncthing = {
+              enable = true;
+              guiAddress = "0.0.0.0:8384";
+              settings = import (self + /lib/syncthing-settings.nix) { dataDir = config.home.homeDirectory; };
+            };
+          };
       };
 
       # Used for backwards compatibility, please read the changelog before changing.
